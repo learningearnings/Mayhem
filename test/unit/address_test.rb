356547b3c263773a73ba40838d007c4148a3955a
David Chapmap <david@isotope11.com>
2012-07-25 15:09:38 +0000

    Fixed a WIP test

--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -1,12 +1,13 @@
 require_relative '../test_helper'
 
+=begin
 describe Address do
   before do
-#    @address = FactoryGirl.build :address
+    @address = FactoryGirl.build :address
   end
 
   it "has a description" do
-#    @address.address1.must_equal '123 Street'
+    @address.address1.must_equal '123 Street'
   end
-
 end
+=end
