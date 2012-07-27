3ca89c790927bc1f69e20993c8fd2499b2b2f19f
David H. Wilkins <dwilkins@conecuh.com>
2012-07-27 08:16:47 -0500

    Adds a FilterConditions class for use with FilterFactory and the associated tests
     * Several references to models are commented out since they don't exist

---
+++ app/models/filter_factory.rb
@@ -0,0 +1,11 @@
+class FilterFactory
+  def find_or_create_by_conditions(conditions)
+    # filter = find_filter(conditions) || new(conditions)
+  end
+
+  def initialize(conditions = nil)
+    # ...
+  end
+
+
+end
