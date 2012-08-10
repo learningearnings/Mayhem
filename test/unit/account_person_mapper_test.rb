352bec11ca23b571a2ff4898d50f239d6a0388f7
Josh Adams <josh@isotope11.com>
2012-08-10 15:52:17 -0500

    Fix AccountPersonMapperTest

--- test/unit/account_person_mapper_test.rb
+++ test/unit/account_person_mapper_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
 describe AccountPersonMapper do
-  subject { AccountPersonMapper.new }
+  subject { AccountPersonMapper.new("STUDENT6") }
 
   it "should know a person_id when given an account" do
-    subject.account_name = "STUDENT6"
     subject.person_id.must_equal 6
   end
 end
