e4f184fb1d52d86ee4a06a04f8e452703d7a4910
David H. Wilkins <dwilkins@conecuh.com>
2012-08-20 15:25:30 -0500

    Change tld_length for our *current* production environment, fix a spelling error Added some exclusions for teachers that should eventually change to school_admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,10 @@ class ApplicationController < ActionController::Base
   def subdomain_required
     return if current_user && current_user.respond_to?(:person) && current_user.person.is_a?(LeAdmin)
     return if current_user && !current_user.respond_to?(:person)
-    if current_user && (request.subdomain.empty? || request.subdomain != home_subdomain) && home_host
+    if current_user && !current_user.person.is_a?(Teacher) && (request.subdomain.empty? || request.subdomain != home_subdomain) && home_host
       token = Devise.friendly_token
       current_user.authentication_token = token
-      my_redirect_url = home_host + "?auth_token=#{token}"
+      my_redirect_url = home_host   + "?auth_token=#{token}"
       current_user.save
       sign_out(current_user)
       redirect_to my_redirect_url
