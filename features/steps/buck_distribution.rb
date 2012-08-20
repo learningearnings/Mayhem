46da3358b91ed8fbd2ca8a932b0320853c9e7a15
Robby Clements <rclements@isotope11.com>
2012-08-20 17:56:09 -0500

    added steps and feature for buck distribution

---
+++ features/steps/buck_distribution.rb
@@ -0,0 +1,31 @@
+class BuckDistribution < Spinach::FeatureSteps
+  Given 'school1 has 7 active students' do
+    @school1 = FactoryGirl.create(:school)
+    7.times do
+      link = FactoryGirl.create(:student_school_link, school: @school1)
+      link.activate
+      link.person.activate
+    end
+  end
+
+  And 'school2 has 3 active students' do
+    @school2 = FactoryGirl.create(:school)
+    3.times do
+      link = FactoryGirl.create(:student_school_link, school: @school2)
+      link.activate
+      link.person.activate
+    end
+  end
+
+  When 'I run the BuckDistributor' do
+    @buck_distributor = BuckDistributor.new
+  end
+
+  Then 'school1 should have 4900 credits' do
+    @school1.balance.must_equal BigDecimal('4900')
+  end
+
+  And 'school2 should have 2100 credits' do
+    @school2.balance.must_equal BigDecimal('2100')
+  end
+end
