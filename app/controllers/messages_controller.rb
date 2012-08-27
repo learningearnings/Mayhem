e728b39a6db62ec281a3e337d9a3f891ba32d3f2
Josh Adams <josh@isotope11.com>
2012-08-27 11:55:37 -0500

    Get messages rendering properly in modal
    - add message_path renderer
    - upgrade to the latest compass_twitter_bootstrap
    - wire up views to pull in proper message path

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,13 @@ class MessagesController < LoggedInController
   def index
     @received_messages = current_person.received_messages
   end
+
+  def show
+    @message = current_person.received_messages.find(params[:id])
+    if request.xhr?
+      render layout: false
+    else
+      render
+    end
+  end
 end
