cdfe70bb48b4dc234f564868d793c7037ac26000
Nicholas Fine <nicholas.fine@gmail.com>
2012-09-06 15:21:15 -0500

    Report Filtering
    - Pass params hash into Report class in Reports::PurchasesController
    - Helper method for generating select options for date filtering
    - View elements for filters (date, teacher, rewards)

--- app/controllers/reports/purchases_controller.rb
+++ app/controllers/reports/purchases_controller.rb
@@ -1,7 +1,7 @@
 module Reports
   class PurchasesController < Reports::BaseController
     def show
-      report = Reports::Purchases.new school: current_school
+      report = Reports::Purchases.new params.merge(school: current_school)
       report.execute!
       render 'show', locals: { report: report }
     end
