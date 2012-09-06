cd57596382a22da5db0cd7ce8b03c4de69858f63
David H. Wilkins <dwilkins@conecuh.com>
2012-09-06 15:28:43 -0500

    Added complex test and added the ib_notify gem

--- app/models/school_store_product_distribution_command.rb
+++ app/models/school_store_product_distribution_command.rb
@@ -6,18 +6,22 @@ class SchoolStoreProductDistributionCommand < ActiveModelCommand
   validates :quantity, :numericality => {:greater_than_or_equal_to => 0}
   validates :retail_price, :numericality => {:greater_than_or_equal_to => 0}
 
-  attr_accessor :master_product, :school, :quantity, :retail_price
+  attr_accessor :master_product, :school, :quantity, :retail_price, :spree_property_class
 
 
   def initialize params={}
     @master_product = params[:master_product]
     @school         = params[:school]
-    @quantity        = params[:quantity]
+    @quantity       = params[:quantity]
     @retail_price   = params[:retail_price]
   end
 
+  def spree_property_class
+    Spree::Property
+  end
+
   def execute!
-    retail_store = Spree::Store.find_by_code(@school.id.to_s)
+    retail_store = @school.store || return
     if retail_store.products.with_property_value('master_product',@master_product.id.to_s).exists?
       retail_product = retail_store.products.with_property_value('master_product',@master_product.id.to_s).first
       ### TODO - Is this legit?
@@ -25,19 +29,16 @@ class SchoolStoreProductDistributionCommand < ActiveModelCommand
       retail_product.master.count_on_hand += @quantity.to_i
       retail_product.master.save
     else
-      retail_price_property = Spree::Property.find_by_name('retail_price');
-      retail_quantity_property = Spree::Property.find_by_name('retail_quantity');
+      retail_price_property = @spree_property_class.find_by_name('retail_price');
+      retail_quantity_property = @spree_property_class.find_by_name('retail_quantity');
 
       retail_product = @master_product.duplicate
-      retail_product.name = @master_product.name # need to set this to avoid "COPY OF ..."
+      retail_product.name = master_product.name # need to set this to avoid "COPY OF ..."
       retail_product.master.price = @retail_price
       retail_product.description = @master_product.description
       retail_product.available_on = Time.now
       retail_product.deleted_at = nil
-      retail_product.permalink = @school.id.to_s + "-" + @master_product.permalink
-      retail_product.meta_description = ""
-      retail_product.meta_keywords = ""
-      retail_product.tax_category_id = nil
+      retail_product.permalink = @school.store_subdomain + "-" + @master_product.permalink
       retail_product.master.count_on_hand = @quantity
       retail_product.product_properties.where(:property_id => retail_price_property.id).each do |p| p.destroy end
       retail_product.product_properties.where(:property_id => retail_quantity_property.id).each do |p| p.destroy end
@@ -49,6 +50,6 @@ class SchoolStoreProductDistributionCommand < ActiveModelCommand
       retail_product.set_property('master_product',@master_product.id.to_s)
       retail_product.master.save # The master variant, not the master_product
       retail_product.save
-      end
+    end
   end
 end
