2feef3aa1908bf3e9961d1e86a773bed178fe382
Josh Adams <josh@isotope11.com>
2012-09-06 12:41:29 -0500

    Add tests for bank.  Not very happy with this class atm.

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,3 +1,6 @@
+require 'action_view'
+require 'active_support/core_ext'
+
 class Bank 
   attr_accessor :on_failure, :on_success
   include ActionView::Helpers::UrlHelper
@@ -10,43 +13,42 @@ class Bank
     if person.main_account(school).balance <= (bucks[:ones] + (bucks[:fives] * 5) + (bucks[:tens] * 10))
       return on_failure.call
     end
-    buck_params = {:person_school_link_id => person.person_school_links.first.id, 
+    buck_params = {:person_school_link => person_school_link(person, school),
                    :expires_at => (Time.now + 45.days) }
     buck_array = []
     bucks[:ones].times do
-      buck_array << OtuCode.create(buck_params.merge :points => BigDecimal.new('1'))
+      buck_array << create_buck(prefix, 1, buck_params)
     end
     bucks[:fives].times do
-      buck_array << OtuCode.create(buck_params.merge :points => BigDecimal.new('5'))
+      buck_array << create_buck(prefix, 5, buck_params)
     end
     bucks[:tens].times do
-      buck_array << OtuCode.create(buck_params.merge :points => BigDecimal.new('10'))
+      buck_array << create_buck(prefix, 10, buck_params)
     end
-    buck_array.map{|x| x.generate_code(prefix)}
     points = buck_array.map{|x| x.points}.sum
-    @credit_manager.purchase_printed_bucks(person.schools.first, person, points)
+    @credit_manager.purchase_printed_bucks(school, person, points)
     return on_success.call
   end
 
   def create_ebucks(person, school, student, prefix, points)
     return @on_failure.call if person.main_account(school).balance <= points
-    buck_params = {:person_school_link_id => person.person_school_links.first.id, 
+    buck_params = {:person_school_link => person_school_link(person, school),
                    :expires_at => (Time.now + 45.days), 
-                   :student_id => student.id, 
-                   :points => BigDecimal.new(points),
+                   :student => student, 
                    :ebuck => true}
-    buck = OtuCode.create(buck_params)
-    buck.generate_code(prefix)
-    @credit_manager.purchase_ebucks(person.schools.first, person, student, points)
-    message = Message.create(:from_id => person.id, 
-                             :to_id => student.id, 
-                             :subject => 'You\'ve been awarded LE Bucks', 
-                             :body => "Click here to claim your award: #{link_to 'Claim Bucks', ("/redeem_bucks?student_id=#{student.id}&code=#{buck.code}")}")
+    buck = create_buck(prefix, points, buck_params)
+    @credit_manager.purchase_ebucks(school, person, student, points)
+    message = message_creator(:from => person, 
+                             :to => student, 
+                             :subject => "You've been awarded LE Bucks", 
+                             :body => "Click here to claim your award: #{link_to 'Claim Bucks', ("/redeem_bucks?student_id=#{student.id}&code=#{buck.code}")}",
+                             :category => 'teacher')
     return on_success.call
   end
 
   # FIXME: Looks like you can claim these twice - that's handled in the
   # controller but I don't think it belongs there
+  # FIXME: This doesn't sound like a responsibility of the bank
   def claim_bucks(student, otu_code)
     if otu_code.is_ebuck?
       @credit_manager.issue_ecredits_to_student(otu_code.school, otu_code.teacher, student, otu_code.points)
@@ -54,5 +56,28 @@ class Bank
       @credit_manager.issue_print_credits_to_student(otu_code.school, otu_code.teacher, student, otu_code.points)
     end
     otu_code.update_attribute(:active, false)
+  end
+
+  protected
+  def buck_creator(params)
+    lambda do |params|
+      OtuCode.create(params)
+    end
+  end
+
+  def message_creator(params)
+    lambda do |params|
+      Message.create params
+    end
+  end
+
+  def create_buck(prefix, points, params)
+    buck = buck_creator.call(params.merge(:points => BigDecimal.new(points)))
+    buck.generate_code(prefix)
+    buck
+  end
+
+  def person_school_link(person, school)
+    person.person_school_links.where(school: school).first
   end
 end
