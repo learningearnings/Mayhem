48032902c3e7936ab055734566c15ec8bb326557
Brandon Adams <badams@isotope11.com>
2012-09-11 18:00:23 -0500

    Added hover and selected states to primary nav.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $(".intro").addClass("selected");
   $(".attention").addClass("selected");
-  
   $(".register-slide, .earn-slide, .enter-slide, .buy-slide, .receive-slide").css('display', 'none');
   $(".step1-slide, .step2-slide, .step3-slide, .step4-slide").css('display', 'none');
 
@@ -94,5 +93,32 @@ $(document).ready(function() {
 	  $(this).addClass("selected");	
 	  $(".attention-teachers").css("display", "block");
 	});
-
+	
+	// Primary Nav Selected
+	if(window.location.href.indexOf("how_it_works") > -1) {
+	 $(".how-it-works").toggleClass("how-it-works-selected")
+	 } 
+	 else if(window.location.href.indexOf("not_logged_in_locker") > -1) {
+		 $(".locker").toggleClass("locker-selected")		
+	}
+	 else if(window.location.href.indexOf("not_logged_in_inbox") > -1) {
+		 $(".inbox").toggleClass("inbox-selected")	
+	}
+	 else if(window.location.href.indexOf("not_logged_in_bank") > -1) {
+		 $(".bank").toggleClass("bank-selected")	
+	}	
+	 else if(window.location.href.indexOf("not_logged_in_play") > -1) {
+		 $(".play").toggleClass("play-selected")	
+	}	
+	 else if(window.location.href.indexOf("not_logged_in_rewards") > -1) {
+		 $(".rewards").toggleClass("rewards-selected")	
+	}	
+	 else if(window.location.href.indexOf("news") > -1) {
+		 $(".news").toggleClass("news-selected")	
+	}	
+	 else if(window.location.href.indexOf("") > -1) {
+		 $(".home").toggleClass("inbox-selected")	
+	}	
+  
+	
 });
