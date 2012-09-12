ac45371aca8ef94bf700121076dd4c6d3ada667b
David H. Wilkins <dwilkins@conecuh.com>
2012-09-11 20:48:08 -0500

    Changed another group from staff to deployer

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,7 +25,7 @@ if env == "production"
   working_directory "/home/deployer/apps/Mayhem/current"
 
   # feel free to point this anywhere accessible on the filesystem
-  user 'deployer', 'staff'
+  user 'deployer', 'deployer'
   shared_path = "/home/deployer/apps/Mayhem/shared"
 
   stderr_path "#{shared_path}/log/unicorn.stderr.log"
