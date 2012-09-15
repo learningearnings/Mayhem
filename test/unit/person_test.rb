0b13120f1035b0064364c1a67a4ab99b9ec2f914
Adam Gamble <adamgamble@gmail.com>
2012-09-15 14:09:28 -0500

    more tests

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -36,4 +36,22 @@ describe Person do
     p.schools.must_include school
     p.classrooms.must_include classroom
   end
+
+  describe "Person#display_name" do
+    it "finds the most recent approved display name" do
+      person = FactoryGirl.create(:person)
+      first_display_name  = person.display_names.create(:display_name => "first")
+      second_display_name = person.display_names.create(:display_name => "second")
+      third_display_name  = person.display_names.creeate(:display_name => "third")
+
+      second_display_name.approve!
+      assert_equal person.display_name, second_display_name
+    end
+
+    it "should return a blank string if there are no display names" do
+      person = FactoryGirl.create(:person)
+      assert person.display_names.empty?
+      assert_equal person.display_name, ""
+    end
+  end
 end
