887a59a48274bcac0421a2c4685e5256c6d5f059
Robby Clements <rclements@isotope11.com>
2012-09-15 15:14:36 -0500

    reverted dashboard back to le admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,15 +1,15 @@
-#ActiveAdmin.register_page "LE Admin Dashboard" do
-ActiveAdmin::Dashboards.build do
+ActiveAdmin.register_page "LE Admin Dashboard" do
+#ActiveAdmin::Dashboards.build do
 
-  #menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  #content :title => proc{ I18n.t("active_admin.dashboard") } do
-  #  div :class => "blank_slate_container", :id => "dashboard_default_message" do
-  #    span :class => "blank_slate" do
-  #      span "Welcome to Active Admin. This is the default dashboard page."
-  #      small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
-  #    end
-  #  end
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    div :class => "blank_slate_container", :id => "dashboard_default_message" do
+      span :class => "blank_slate" do
+        span "Welcome to Active Admin. This is the default dashboard page."
+        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
+      end
+    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -30,5 +30,5 @@ ActiveAdmin::Dashboards.build do
     #     end
     #   end
     # end
-  #end # content
+  end # content
 end
