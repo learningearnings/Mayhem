30ba7bc006307c1f7474c5072839d3f53b21fb8f
David H. Wilkins <dwilkins@conecuh.com>
2012-09-17 22:24:07 -0500

    Made it not create *another* display_name if update was pressed and the display name was the same as the last approved.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,11 +13,12 @@ class PeopleController < ApplicationController
   def update
     @person = Person.find(params[:id])
     @person.avatar = Avatar.find(params[:avatar_id]) if !params[:avatar_id].blank?
-    if @person.update_attributes(params[:student])
-        flash[:notice] = "#{@person.type} Avatar updated."
-        redirect_to main_app.root_path
+    if (!params[:student][:display_name].blank? && params[:student][:display_name] != @person.display_name) &&  @person.update_attributes(params[:student])
+      flash[:notice] = "#{@person.type} profile updated."
+      redirect_to main_app.root_path
     else
-      flash[:notice] = 'Avatar not updated.'
+      @avatars = Avatar.page params[:page]
+      flash[:notice] = "#{@person.type} profile not updated."
       render :edit
     end
   end
