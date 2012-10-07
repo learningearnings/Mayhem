8844275b9dfe25af6f0e032f109551d109803c75
David H. Wilkins <dwilkins@conecuh.com>
2012-10-07 12:23:01 -0500

    Change to hopefully trigger Jenkins...

--- lib/importers/oldschema/old_school.rb
+++ lib/importers/oldschema/old_school.rb
@@ -2,6 +2,8 @@ class OldSchool < ActiveRecord::Base
   establish_connection(:legacy)
   self.table_name = 'tbl_schools'
   self.primary_key = 'schoolID'
+
+
 #  scope :school_subset,  where(:schoolID => [91,215,229,221,98,830,483,412,96,633,873,538,121,94,277]).where('status_id = 200 and ad_profile < 21')
   scope :school_subset,  where(:schoolID => [98,215,221,830,483,523]).where('status_id = 200 and ad_profile < 20')
   scope :school_subset1,  where(:schoolID => [98,221]).where('status_id = 200')
