864a1aaa6fc051583058a6553ee8f2be24e63a06
David H. Wilkins <dwilkins@conecuh.com>
2012-10-07 11:47:52 -0500

    Added multiple worker capability

--- lib/importers/oldschema/old_school.rb
+++ lib/importers/oldschema/old_school.rb
@@ -5,7 +5,7 @@ class OldSchool < ActiveRecord::Base
 
 #  scope :school_subset,  where(:schoolID => [91,215,229,221,98,830,483,412,96,633,873,538,121,94,277]).where('status_id = 200 and ad_profile < 21')
   scope :school_subset,  where(:schoolID => [98,215,221,830,483,523]).where('status_id = 200 and ad_profile < 20')
-  scope :school_subset1,  where(:schoolID => [98,221,830,483,523]).where('status_id = 200')
+  scope :school_subset1,  where(:schoolID => [98,221]).where('status_id = 200')
   scope :school_subset2,  where(:schoolID => [215]).where('status_id = 200')
   scope :school_subset3,  where(:schoolID => [830,483]).where('status_id = 200')
   scope :school_subset4,  where(:schoolID => [523]).where('status_id = 200')
