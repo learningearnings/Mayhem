f062d55a2a1d2f6732d489e1b9ac9d4e4731fd59
David H. Wilkins <dwilkins@conecuh.com>
2012-10-16 08:40:41 -0500

    Oops - removed a binding.pry that lingered

--- app/controllers/restock_controller.rb
+++ app/controllers/restock_controller.rb
@@ -3,7 +3,6 @@ class RestockController < LoggedInController
     @pending_shipments = Spree::Order.joins(:user).where('spree_users.id' => current_user).where(:state => ['transmitted','pending','printed','complete','shipped']).where("#{Spree::Order.table_name}.created_at > ?",Time.now - 2.weeks ).order("#{Spree::Order.table_name}.created_at desc").limit(4)
     @transmitted = Spree::Order.joins(:user).where('spree_users.id' => current_user).where(:state => ['transmitted']).where("#{Spree::Order.table_name}.created_at > ?",Time.now - 2.weeks ).exists?
     @order = Spree::Order.joins(:user).where('spree_users.id' => current_user).where(:state => ['cart']).first
-    binding.pry
     respond_with(@pending_shipments, @order,@transmitted)
   end
 end
