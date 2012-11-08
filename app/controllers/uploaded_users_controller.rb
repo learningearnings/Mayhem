cec43d26711ee000813d00e07466c2b5a5bd0c4b
David H. Wilkins <dwilkins@conecuh.com>
2012-11-07 19:41:03 -0600

    fixed the uploaded users controller

--- app/controllers/uploaded_users_controller.rb
+++ app/controllers/uploaded_users_controller.rb
@@ -1,4 +1,38 @@
 class UploadedUsersController < LoggedInController
   before_filter :ensure_le_admin!
 
+  def index
+    @uploaded_users = UploadedUser.all
+  end
+
+  def edit
+    @uploaded_user = UploadedUser.find(params[:id])
+  end
+
+  def show
+    @uploaded_user = UploadedUser.find(params[:id])
+  end
+
+  def new
+    @uploaded_user = UploadedUser.new
+  end
+
+
+  def create
+    @uploaded_user = UploadedUser.new(params[:uploaded_user])
+    if @uploaded_user.save
+      redirect_to uploaded_user_path(@uploaded_user), :notice => "User created!"
+  else
+      render :action => "new"
+    end
+  end
+
+  def destroy
+    uploaded_user = UploadedUser.find(params[:id])
+    uploaded_user.destroy
+    flash[:notice] = "You just deleted a user"
+    redirect_to uploaded_users_path
+  end
+
+
 end
