730429e4f34fd21a6dfa8ec2b497fba44883851a
David H. Wilkins <dwilkins@conecuh.com>
2012-11-20 14:39:11 -0600

    A few changes to fix charity problems on production
     * better use of compass-twitter-bootstrap
     * better handling of jacked svg property on a spree::product
     * compile ie_noprint.css.scss in production
     * added stylesheet ie_noprint to the responsive layout

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -2,7 +2,7 @@ class CharitiesController < LoggedInController
   layout 'resp_application'
 
   def index
-    charities = Plutus::Transaction.for_person(current_person.id).with_main_account.with_spree_products.merge(Spree::Product.with_property_value('reward_type','charity')).page(params[:page]).per(4)
+    charities = Plutus::Transaction.for_person(current_person.id).with_main_account.with_spree_products.merge(Spree::Product.with_property_value('reward_type','charity')).order('plutus_transactions.created_at desc').page(params[:page]).per(4)
     render 'index', locals: { charities: charities }
   end
   def print
