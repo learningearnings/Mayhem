cfa6b21a1a4647507787feaee78f2c767d7988cf
David H. Wilkins <dwilkins@conecuh.com>
2012-12-14 14:01:55 -0600

    Fixed an issue with activity report
     * Unnecessary .joins(...) that were causing the failure
     * Failure was "undefined method 'chain' for nil:NIL class"
     * added a couple of scopes to person - with_transactions_between and with_transactions_since
     * Added a gnarly .select(...) to the activity report using the above scopes
     * The .select(...) I added keeps the sql to just one line instead of
       fetching back spree_users.username and the transaction activity

--- app/models/plutus/account_decorator.rb
+++ app/models/plutus/account_decorator.rb
@@ -1,3 +1,3 @@
 Plutus::Account.class_eval do
-  has_one :person_account_link, :foreign_key => :plutus_account_id, :class_name => 'PersonAccountLink', :inverse_of => :account
+  has_one :person_account_link, :foreign_key => :plutus_account_id, :class_name => 'PersonAccountLink', :inverse_of => :plutus_account
 end
