2ac9c77cf782a4ebe26d297c7e574ec5ed3fc04f
Josh Adams <josh@isotope11.com>
2013-08-07 15:25:04 -0500

    Add ability to distribute credits to classroom selectively
    Let the teacher pick the amount of credits to send to each student in a
    given classroom

--- app/controllers/mixins/banks.rb
+++ app/controllers/mixins/banks.rb
@@ -58,7 +58,7 @@ module Mixins
     end
 
     def issue_ebucks_to_student(student)
-      @bank.create_ebucks(person, current_school, student, 'AL', BigDecimal(params[:points].gsub(/[^\d]/, '')))
+      @bank.create_ebucks(person, current_school, student, 'AL', BigDecimal(params[:credits][student.id.to_s].gsub(/[^\d]/, '')))
     end
   end
 end
