0288c6e8cb1d9022f1ab3d7a0cb8b99e411f07aa
Jimmy Benson <jimmybnsn@gmail.com>
2013-08-09 06:39:42 -0500

    Updated after bundle install and rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -996,12 +996,14 @@ ActiveRecord::Schema.define(:version => 20130805215100) do
   end
 
   create_table "spree_tax_rates", :force => true do |t|
-    t.decimal  "amount",            :precision => 8, :scale => 5
+    t.decimal  "amount",             :precision => 8, :scale => 5
     t.integer  "zone_id"
     t.integer  "tax_category_id"
-    t.datetime "created_at",                                                         :null => false
-    t.datetime "updated_at",                                                         :null => false
-    t.boolean  "included_in_price",                               :default => false
+    t.datetime "created_at",                                                          :null => false
+    t.datetime "updated_at",                                                          :null => false
+    t.boolean  "included_in_price",                                :default => false
+    t.string   "name"
+    t.boolean  "show_rate_in_label",                               :default => true
   end
 
   create_table "spree_taxonomies", :force => true do |t|
@@ -1101,6 +1103,7 @@ ActiveRecord::Schema.define(:version => 20130805215100) do
     t.integer  "count_on_hand",                               :default => 0,     :null => false
     t.decimal  "cost_price",    :precision => 8, :scale => 2
     t.integer  "position"
+    t.integer  "lock_version",                                :default => 0
   end
 
   add_index "spree_variants", ["product_id"], :name => "index_variants_on_product_id"
