da70f4634a18df261f4d9ba420c839283be9d9da
Josh Adams <josh@isotope11.com>
2013-08-19 13:56:55 -0500

    Fix test for classroom details importer

--- test/unit/importers/le/classroom_details_importer_test.rb
+++ test/unit/importers/le/classroom_details_importer_test.rb
@@ -8,16 +8,38 @@ describe Importers::Le::ClassroomDetailsImporter do
   let(:file_path){ File.expand_path("../../../../support/files/le/classroom_details.csv", __FILE__) }
   let(:fake_classroom){ Object.new }
   let(:fake_person){ Object.new }
+  let(:fake_person_school_link){ Object.new }
+  let(:fake_person_school_classroom_link){ Object.new }
   subject { Importers::Le::ClassroomDetailsImporter.new(file_path.to_s) }
+
+  before do
+    fake_person_school_link.stubs(:active?).returns(false)
+    fake_person_school_classroom_link.stubs(:active?).returns(false)
+    fake_person_school_link.stubs(:activate!)
+    fake_person_school_classroom_link.stubs(:activate!)
+    fake_person.stubs(:person_school_links).returns([fake_person_school_link])
+    fake_person.stubs(:person_school_classroom_links).returns([fake_person_school_classroom_link])
+    fake_person.stubs(:<<)
+    Person.stubs(:where).with(legacy_user_id: "545").returns([fake_person])
+    Classroom.stubs(:where).with(legacy_classroom_id: "257").returns([fake_classroom])
+  end
 
   it "takes a filename as its only argument" do
     subject.file_path.must_equal file_path.to_s
   end
 
   it "adds each person to the corresponding classroom" do
-    Person.stubs(:where).with(legacy_user_id: "545").returns([fake_person])
-    Classroom.stubs(:where).with(legacy_classroom_id: "257").returns([fake_classroom])
     fake_person.expects(:<<).with(fake_classroom)
+    subject.call
+  end
+
+  it "activates the school links" do
+    fake_person_school_link.expects(:activate!)
+    subject.call
+  end
+
+  it "activates the classroom links" do
+    fake_person_school_classroom_link.expects(:activate!)
     subject.call
   end
 end
