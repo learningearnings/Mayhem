6f79bb90ec8bef1d15331675d8b366fbd35d54bc
Josh Adams <josh@isotope11.com>
2013-08-28 15:17:10 -0500

    Add new app server to production cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :branch,  "origin/master"
-role :web,    "192.241.248.83"
-role :app,    "192.241.248.83"
+role :web,    "192.241.248.83", "192.241.249.30"
+role :app,    "192.241.248.83", "192.241.249.30"
 role :db,     "192.241.248.83", primary: true
