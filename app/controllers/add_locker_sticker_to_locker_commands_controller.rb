091b6cecff869d1f928d345ef22508d28b310521
Josh Adams <josh@isotope11.com>
2013-09-10 16:02:20 -0500

    Tweak locker with jimmy's requests

--- app/controllers/add_locker_sticker_to_locker_commands_controller.rb
+++ app/controllers/add_locker_sticker_to_locker_commands_controller.rb
@@ -3,6 +3,7 @@ class AddLockerStickerToLockerCommandsController < LoggedInController
     # FIXME: This isn't really a command yet
     sticker = Sticker.find(params[:id])
     link = current_person.locker << sticker
+    link.update_attributes(x: params[:x], y: params[:y])
     render json: { id: link.id }
   end
 end
--- app/models/locker_sticker_link.rb
+++ app/models/locker_sticker_link.rb
@@ -2,6 +2,8 @@ class LockerStickerLink < ActiveRecord::Base
   belongs_to :locker
   belongs_to :sticker
 
+  attr_accessible :x, :y
+
   validates :locker_id, presence: true, numericality: true
   validates :sticker_id, presence: true, numericality: true
   validates :x, presence: true, numericality: true
