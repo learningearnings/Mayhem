509bd24295f6df5994653364c50dff8e0b4fb156
rclements <rclements@isotope11.com>
2013-09-23 17:29:19 -0500

    working on fixing reward creation

--- app/models/spree/search/filter.rb
+++ app/models/spree/search/filter.rb
@@ -18,26 +18,25 @@ module Spree::Search
     protected
     def get_base_scope
       # Copied from spree-multi-domain/lib/spree/search/multi_domain.rb
-
       base_scope = @cached_product_group ? @cached_product_group.products.active : Spree::Product.active
-      base_scope = base_scope.by_store(current_store_id) if current_store_id
-      base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
-      base_scope = base_scope.not_excluded(current_school) if current_school
-
-      base_scope = get_products_conditions_for(base_scope, keywords) unless keywords.blank?
       # Leadmins get to see out of stock products
-      unless  (@current_user && @current_user.person.is_a?(LeAdmin))
+      # don't use filters with LeAdmins
+
+
+      unless (@current_user && @current_user.person.is_a?(LeAdmin))
+        base_scope = base_scope.by_store(current_store_id) if current_store_id
+        base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
+        base_scope = base_scope.not_excluded(current_school) if current_school
+
+        base_scope = get_products_conditions_for(base_scope, keywords) unless keywords.blank?
         base_scope = base_scope.on_hand unless Spree::Config[:show_zero_stock_products]
+        base_scope.with_filter(@filters)
+        base_scope = base_scope.not_shipped_for_school_inventory
+        if @current_person
+          base_scope = base_scope.above_min_grade(@current_person.grade).below_max_grade(@current_person.grade)
+        end
       end
       base_scope = add_search_scopes(base_scope)
-      # don't use filters with LeAdmins
-      unless  (@current_user && @current_user.person.is_a?(LeAdmin))
-        base_scope.with_filter(@filters)
-      end
-      if @current_person
-        base_scope = base_scope.above_min_grade(@current_person.grade).below_max_grade(@current_person.grade)
-      end
-      base_scope = base_scope.not_shipped_for_school_inventory
       base_scope
     end
 
