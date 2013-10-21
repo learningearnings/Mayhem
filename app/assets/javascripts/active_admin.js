18de72a2afa76a88458b337911a92405acd688e6
Seth Messer <seth.messer@gmail.com>
2013-10-21 07:59:55 -0500

    Fix Active Admin IE issues. (specifically the range slider)

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,6 +2,13 @@
 //= require active_admin/base
 //= require ckeditor/init
 //= require chosen-jquery
+//= require modernizr-special
+//= require webshims/polyfiller
+
+$.webshims.setOptions('forms-ext', {
+  types: 'range date time number month color'
+});
+$.webshims.polyfill();
 
 $(document).ready(function(){
   $('.chosen-input').chosen();
