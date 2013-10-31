f8c803fa52f03b8379d3ca9cfd27330b4ac6ab25
Josh Adams <josh@isotope11.com>
2013-10-30 22:05:58 -0500

    More logging to track down subdomain issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -134,9 +134,9 @@ class ApplicationController < ActionController::Base
   helper_method :actual_subdomain
 
   def not_at_home
+    Rails.logger.warn "request.subdomain: #{request.subdomain}"
     return true if request.subdomain.empty?
     first_subdomain = actual_subdomain
-    Rails.logger.warn "request.subdomain: #{request.subdomain}"
     Rails.logger.warn "first subdomain: #{first_subdomain}"
     return first_subdomain != home_subdomain
   end
