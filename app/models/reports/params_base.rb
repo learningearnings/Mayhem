a41aeeda4e6493fdbceb9fe2d85a9069a16da238
Adam Gamble <adamgamble@gmail.com>
2013-11-11 10:35:44 -0600

    Fixed a 500 I caused

--- app/models/reports/params_base.rb
+++ app/models/reports/params_base.rb
@@ -7,9 +7,11 @@ module Reports
     attr_accessor :per_page, :page, :paginate
 
     def initialize options_in = {}
-      @paginate = options_in["reports_purchases_params"].fetch(:paginate, false)
-      @per_page = options_in.fetch(:per_page, 50).to_i
-      @page     = options_in.fetch(:page, 1).to_i
+      unless options_in
+        @paginate = options_in["reports_purchases_params"].fetch(:paginate, false)
+        @per_page = options_in.fetch(:per_page, 50).to_i
+        @page     = options_in.fetch(:page, 1).to_i
+      end
     end
 
     def persisted?
