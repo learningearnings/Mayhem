c3baf9739ad6c19bb0b0eff3ec5036afdc3526fb
Adam Gamble <adamgamble@gmail.com>
2013-11-13 16:31:06 -0600

    User creation is working with school links

--- app/controllers/teachers/bulk_students_controller.rb
+++ app/controllers/teachers/bulk_students_controller.rb
@@ -15,6 +15,9 @@ module Teachers
     def create
       @batch_student_creator = BatchStudentCreator.new(params["students"])
       if @batch_student_creator.call
+        @batch_student_creator.students.each do |student|
+          PersonSchoolLink.find_or_create_by_person_id_and_school_id(student.id, current_person.schools.first.id)
+        end
         flash[:notice] = "Students Created!"
         redirect_to action: :index
       else
--- app/models/batch_student_creator.rb
+++ app/models/batch_student_creator.rb
@@ -11,12 +11,21 @@ class BatchStudentCreator
   def call
     ActiveRecord::Base.transaction do
       @student_params.each do |student_param|
+        user_params = student_param.delete(:user)
+        student_param.merge! password_confirmation: student_param[:password]
         student = @student_class.new(student_param)
+        user = Spree::User.new
+        student.user = user
+        user.update_attributes(user_params)
+        user.save
         student.save
         students << student
-        raise ActiveRecord::Rollback unless student.valid?
+      end
+      students.each do |student|
+        raise ActiveRecord::Rollback unless student.valid? || student.user.valid?
       end
     end
+    binding.pry
     return false if students.select{|s| !s.valid? }.any?
     return true
   end
