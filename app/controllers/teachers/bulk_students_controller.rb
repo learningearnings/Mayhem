96aea5aa35dc3b9f6070624e38332210396beb1d
Adam Gamble <adamgamble@gmail.com>
2013-11-14 08:27:25 -0600

    You can now assign classrooms to new students in the manage students

--- app/controllers/teachers/bulk_students_controller.rb
+++ app/controllers/teachers/bulk_students_controller.rb
@@ -13,11 +13,8 @@ module Teachers
     end
 
     def create
-      @batch_student_creator = BatchStudentCreator.new(params["students"])
+      @batch_student_creator = BatchStudentCreator.new(params["students"], current_person.schools.first)
       if @batch_student_creator.call
-        @batch_student_creator.students.each do |student|
-          PersonSchoolLink.find_or_create_by_person_id_and_school_id(student.id, current_person.schools.first.id)
-        end
         flash[:notice] = "Students Created!"
         redirect_to action: :index
       else
--- app/models/batch_student_creator.rb
+++ app/models/batch_student_creator.rb
@@ -1,16 +1,18 @@
 # This class is used to create users in the system in bulk.
 class BatchStudentCreator
-  attr_reader :students
+  attr_reader :students, :school
 
-  def initialize student_params, student_class=Student
-    @students = []
-    @student_params = student_params
-    @student_class = student_class
+  def initialize student_params,school=nil, student_class=Student
+    @students       = []
+    @student_params = student_params.dup
+    @student_class  = student_class
+    @school         = school
   end
 
   def call
     ActiveRecord::Base.transaction do
       @student_params.each do |student_param|
+        classroom_id = student_param.delete(:classroom_id)
         user_params = student_param.delete(:user)
         student_param.merge! password_confirmation: student_param[:password]
         student = @student_class.new(student_param)
@@ -19,6 +21,11 @@ class BatchStudentCreator
         user.update_attributes(user_params)
         user.save
         student.save
+        if school
+          psl = PersonSchoolLink.find_or_create_by_person_id_and_school_id(student.id, school.id)
+          pscl = PersonSchoolClassroomLink.find_or_create_by_classroom_id_and_person_school_link_id(classroom_id, psl.id)
+          pscl.activate
+        end
         students << student
       end
       students.each do |student|
