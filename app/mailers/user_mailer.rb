023e4384728c9d2ce3c9813c0a05dbc49d301351
Robby Clements <rclements@isotope11.com>
2013-11-25 11:05:51 -0600

    working on mailer for teacher admin messages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,17 @@
 class UserMailer < ActionMailer::Base
   default from: "admin@learningearnings.com"
 
+  def teacher_admin_email(message)
+    @message = message
+    @teacher = Person.find @message.to_id
+    mail(:to => 'rclements@gmail.com', :subject => "#{@message.subject}")
+    #mail(:to => 'theteam@learningearnings.com', :subject => "#{@message.subject}")
+  end
+
   def teacher_request_email(teacher)
     @teacher = teacher
     @url  = "http://learningearnings.com/login"
-    mail(:to => 'rclements@gmail.com', :subject => "Teacher registration approval.")
+    mail(:to => 'theteam@learningearnings.com', :subject => "Teacher registration approval.")
   end
 
   def teacher_approval_email(teacher)
