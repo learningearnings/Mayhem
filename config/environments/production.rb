b630ad941034fb59e8fe547681b70f9f43e9d070
Adam Gamble <adamgamble@gmail.com>
2013-12-05 14:21:56 -0600

    changed some configuration options for demo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,10 @@ Leror::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
