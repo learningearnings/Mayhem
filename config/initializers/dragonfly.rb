4a6bf2bc4f95915655ae4c9b28500a1dcd9d5659
Seth Messer <seth.messer@gmail.com>
2013-12-09 11:22:25 -0600

    Set dragonfly s3 datastore protocol to https

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -5,6 +5,7 @@ app.configure do |c|
   c.allow_fetch_url = true
 end
 app.datastore = Dragonfly::DataStorage::S3DataStore.new({
+  url_scheme: "https",
   bucket_name: ENV['LE_S3_BUCKET'],
   access_key_id: ENV['LE_S3_ACCESS_KEY'],
   secret_access_key: ENV['LE_S3_SECRET_ACCESS_KEY'],
