b1a73d69796be6b1bc4cca5eda12565681473f35
Seth Messer <seth.messer@gmail.com>
2013-12-09 14:03:27 -0600

    Change spree to use https for it's s3 config as well

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -4,12 +4,10 @@ app.configure do |c|
   c.allow_fetch_file = true
   c.allow_fetch_url = true
 end
-
-app.datastore = Dragonfly::DataStorage::S3DataStore.new
-app.datastore.configure do |c|
-  c.bucket_name = ENV['LE_S3_BUCKET']
-  c.access_key_id = ENV['LE_S3_ACCESS_KEY']
-  c.secret_access_key = ENV['LE_S3_SECRET_ACCESS_KEY']
-  c.region = 'us-west-2'
-  c.url_scheme = 'https'
-end
+app.datastore = Dragonfly::DataStorage::S3DataStore.new({
+  bucket_name: ENV['LE_S3_BUCKET'],
+  access_key_id: ENV['LE_S3_ACCESS_KEY'],
+  secret_access_key: ENV['LE_S3_SECRET_ACCESS_KEY'],
+  region: 'us-west-2',
+  url_scheme: 'https'
+})
