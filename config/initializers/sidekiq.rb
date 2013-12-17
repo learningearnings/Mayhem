d7187911132ec8ffb9c29da340085f75f72e3c3e
rclements <rclements@isotope11.com>
2013-12-17 15:00:51 -0600

    removed sidekiq initializer

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://192.241.241.213:7372/12'}
-
-  database_url = '192.241.241.213'
-  if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
-    ActiveRecord::Base.establish_connection
-  end
-end
