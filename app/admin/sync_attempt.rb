c148b9f1d9f11d784797fe2fb5df6dc1aa51a517
Adam Gamble <adamgamble@gmail.com>
2014-02-19 12:55:47 -0600

    Added time sync took

--- app/admin/sync_attempt.rb
+++ app/admin/sync_attempt.rb
@@ -1,6 +1,9 @@
 ActiveAdmin.register SyncAttempt do
   index do
     column :created_at
+    column :total_time do |object|
+      distance_of_time_in_words(object.created_at, object.updated_at)
+    end
     column :district_guid
     column :status
     column :error
