566605fe4a88d1b0f53b9173c51c370ebc8337f0
Robby Clements <rclements@isotope11.com>
2014-02-20 08:26:16 -0600

    add guid check for actions in active admin for schools

--- app/admin/common_person_config.rb
+++ app/admin/common_person_config.rb
@@ -161,6 +161,16 @@ module CommonPersonConfig
           t.updated_at.strftime("%m/%d/%Y") if t.updated_at
         end
         default_actions
+        #column :actions do |resource|
+        #  links = link_to I18n.t('active_admin.view'), resource_path(resource)
+        #  links += ' '
+        #  if !resource.district_guid.present?
+        #    links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource)
+        #    links += ' '
+        #  end
+        #    links += link_to "Delete", resource_path(resource), :confirm => 'Are you sure?', :method => :delete
+        #  links
+        #end
       end
       controller do
         skip_before_filter :add_current_store_id_to_params
--- app/admin/schools.rb
+++ app/admin/schools.rb
@@ -27,7 +27,17 @@ ActiveAdmin.register School do
     column :store_subdomain
     column "STI District GUID", :district_guid
     column "STI id", :sti_id
-    default_actions
+    #default_actions
+    column :actions do |resource|
+      links = link_to I18n.t('active_admin.view'), resource_path(resource)
+      links += ' '
+      if !resource.district_guid.present?
+        links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource)
+        links += ' '
+      end
+      links += link_to "Delete", resource_path(resource), :confirm => 'Are you sure?', :method => :delete
+      links
+    end
   end
 
   form :partial => 'form'
