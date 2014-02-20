c5e34d1c20cbb4737358c4b3b7f2e53f30db85a4
Robby Clements <rclements@isotope11.com>
2014-02-20 08:27:04 -0600

    added guid action check to common config

--- app/admin/common_person_config.rb
+++ app/admin/common_person_config.rb
@@ -160,17 +160,17 @@ module CommonPersonConfig
         column "Updated", :updated_at do |t|
           t.updated_at.strftime("%m/%d/%Y") if t.updated_at
         end
-        default_actions
-        #column :actions do |resource|
-        #  links = link_to I18n.t('active_admin.view'), resource_path(resource)
-        #  links += ' '
-        #  if !resource.district_guid.present?
-        #    links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource)
-        #    links += ' '
-        #  end
-        #    links += link_to "Delete", resource_path(resource), :confirm => 'Are you sure?', :method => :delete
-        #  links
-        #end
+        #default_actions
+        column :actions do |resource|
+          links = link_to I18n.t('active_admin.view'), resource_path(resource)
+          links += ' '
+          if !resource.district_guid.present?
+            links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource)
+            links += ' '
+          end
+            links += link_to "Delete", resource_path(resource), :confirm => 'Are you sure?', :method => :delete
+          links
+        end
       end
       controller do
         skip_before_filter :add_current_store_id_to_params
