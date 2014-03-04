d93c1e0085da85f3a0aae267bf50efeab50df993
Josh Adams <josh@isotope11.com>
2014-03-03 19:48:31 -0600

    Leave old ruby version around for benchmarking

--- app/models/rewards_filter.rb
+++ app/models/rewards_filter.rb
@@ -19,4 +19,18 @@ module RewardsFilter
     product_ids = ActiveRecord::Base.connection.execute(product_ids_query.to_sql).map{|result| result["id"] }
     Spree::Product.where(id: product_ids)
   end
+
+  def self.ruby_by_classroom(person, products)
+    return products unless person.is_a?(Student)
+    classrooms = person.classrooms.pluck(:id)
+    products.reject! do |product|
+      # Products that have no classrooms should not be rejected
+      next if product.classrooms.empty?
+      # If there is an intersection between the products classrooms and my classrooms, don't reject
+      (product.classrooms.pluck(:id) & classrooms).any? ? false : true
+    end
+    # To fix pagination we need an active record relation, not an array
+    # Why are you laughing?
+    products = Spree::Product.where(:id => products.map(&:id))
+  end
 end
