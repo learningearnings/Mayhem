f0f9d8b1eb55529b4fe45a69cd3045ddabb43bfa
Adam Gamble <adamgamble@gmail.com>
2014-03-05 09:41:16 -0600

    Modified the buck distributor to give 25 dollars per day per active student.
    Ripped out the proration. It now prorates by default.
    
    Updated the tests for this new behavior

--- app/models/buck_distributor.rb
+++ app/models/buck_distributor.rb
@@ -1,8 +1,9 @@
 class BuckDistributor
-  def initialize(schools=School.all, credit_manager=CreditManager.new, should_prorate=false)
+  DAILY_STUDENT_AMOUNT = 25
+
+  def initialize(schools=School.all, credit_manager=CreditManager.new)
     @schools = schools
     @credit_manager = credit_manager
-    @should_prorate = should_prorate
   end
 
   def run
@@ -21,15 +22,11 @@ class BuckDistributor
     @credit_manager.issue_credits_to_school school, amount_for_school(school)
   end
 
+  # 25 dollars per student per day
   def amount_for_school school
-    initial_amount = 700 * school.students.recently_logged_in.count
-    if @should_prorate
-      days_in_month = Time.days_in_month(Time.now.month)
-      daily_amount = BigDecimal(initial_amount) / BigDecimal(days_in_month)
-      days_left_in_month = (days_in_month - Time.now.day) + 1
-      prorated_amount = (days_left_in_month * daily_amount).round(0, :up).to_i
-    end
-    @should_prorate ? prorated_amount : initial_amount
+    days_in_month = Time.days_in_month(Time.now.month)
+    days_left_in_month = (days_in_month - Time.now.day) + 1
+    DAILY_STUDENT_AMOUNT * days_left_in_month * school.students.recently_logged_in.count
   end
 
   def handle_teachers
