e2d3dcfe81001477c3e209a1feb3ef796029defa
Adam Gamble <adamgamble@gmail.com>
2014-03-06 00:32:40 -0600

    Modify deploy to deploy sandbox from sti_beta

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,5 +1,5 @@
 set :rails_env, "sandbox"
-set :branch,  "sandbox"
+set :branch,  "sti_beta"
 role :web,    "162.243.39.36"
 role :app,    "162.243.39.36"
 role :db,     "162.243.39.36", primary: true
