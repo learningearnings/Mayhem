d847d5726e903d64153ebadec8bae5d9f6e09acf
Adam Gamble <adamgamble@gmail.com>
2014-03-06 09:59:29 -0600

    Changed qa site to deploy from sti_beta

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,5 +1,5 @@
 set :rails_env, "qa"
-set :branch,  "sandbox"
+set :branch,  "sti_beta"
 role :web,    "162.243.202.62"
 role :app,    "162.243.202.62"
 role :db,     "162.243.202.62", primary: true
