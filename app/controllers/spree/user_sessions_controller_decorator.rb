3acd14f5c9db5c4819071a93018560913a90aea5
Adam Gamble <adamgamble@gmail.com>
2014-03-13 09:43:12 -0500

    Moved the last_logged_in_school_id cookie to AFTER the destroy of the session

--- app/controllers/spree/user_sessions_controller_decorator.rb
+++ app/controllers/spree/user_sessions_controller_decorator.rb
@@ -1,11 +1,13 @@
 Spree::UserSessionsController.class_eval do
+  before_filter :set_current_school_id, only: [:destroy]
+  after_filter :set_school_id_cookie, only: [:destroy]
+
   def create
     authenticate_user!
     session[:last_school_id] = params[:user]["school_id"]
     session[:current_school_id] = params[:user]["school_id"]
 
     if user_signed_in?
-      cookies[:last_logged_in_school_id] = { :value => params[:user]["school_id"], :expires => 1.year.from_now, :domain => ".learningearnings.com"}
       respond_to do |format|
         format.html {
           flash.notice = t(:logged_in_succesfully)
@@ -24,5 +26,13 @@ Spree::UserSessionsController.class_eval do
 
   def new
     redirect_to main_app.page_path('home')
+  end
+  private
+  def set_current_school_id
+    @current_school_id = current_school.id
+  end
+
+  def set_school_id_cookie
+    cookies[:last_logged_in_school_id] = { :value => @current_school_id, :expires => 1.year.from_now, :domain => ".learningearnings.com"}
   end
 end
