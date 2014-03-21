c2fe53023ba1fa8e80c33ad72ee05b11527fc827
Adam Gamble <adamgamble@gmail.com>
2014-03-21 10:06:24 -0400

    addded sidekiq to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 set :branch,    "staging"
 set :rails_env, "staging"
 role :web,      "mayhemstaging.isotopecloud.com"
+role :sidekiq,  "mayhemstaging.isotopecloud.com"
 role :app,      "mayhemstaging.isotopecloud.com"
 role :db,       "mayhemstaging.isotopecloud.com", primary: true
