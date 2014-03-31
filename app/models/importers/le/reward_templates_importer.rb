c2aa3890b43988df571e6de15c6809507901c4cb
rclements <rclements@isotope11.com>
2014-03-31 14:15:14 -0500

    adding reward templates controller and importer

---
+++ app/models/importers/le/reward_templates_importer.rb
@@ -0,0 +1,91 @@
+require 'csv'
+require 'logger'
+require 'forwardable'
+
+
+#     i = Importers::Le::RewardTemplatesImporter.new("/home/robby/Desktop/le/rewards-templates.csv", "/home/robby/Desktop/le/reward_templates/")
+#     i.call
+#
+# It expects to be passed a directory where the images whose filenames are
+# represented in the master_rewards.csv file can be found.  Should be a string,
+# ending in a slash, because I'm lazy.
+module Importers
+  class Le
+    class RewardTemplatesImporter
+      extend Forwardable
+      def_delegators :@logger, :warn, :info, :debug
+
+      attr_reader :rewards_file_path, :log_file_path, :images_directory
+      def initialize(rewards_file_path, images_directory, log_file_path='/tmp/le_importer.log')
+        @rewards_file_path = rewards_file_path
+        @images_directory = images_directory
+        @log_file = File.open(log_file_path, 'a')
+        @log_file_path = log_file_path
+        @logger = Logger.new(@log_file)
+      end
+
+      def call
+        begin
+          #ActiveRecord::Base.transaction do
+            run
+          #end
+        ensure
+          @log_file.close
+        end
+      end
+
+      protected
+      def run
+        rewards_data.each do |datum|
+          create_template_for(datum)
+        end
+      end
+
+      def create_template_for(datum)
+        reward_template = RewardTemplate.new
+        reward_template.name = datum[:name]
+        reward_template.min_grade = datum[:min_grade]
+        reward_template.max_grade = datum[:max_grade]
+        reward_template.save
+        #if(datum[:image])
+        #  reward_template.images.create(attachment: datum[:image])
+        #end
+      end
+
+      def rewards_data
+        parsed_rewards_doc.map do |reward|
+          {
+            name: reward["Name"],
+            min_grade: reward["Min Grade"],
+            max_grade: reward["Max Grade"],
+            image: image_for(reward["Image Name"])
+          }
+        end
+      end
+
+      def parsed_rewards_doc
+        parsed_doc(rewards_file_data)
+      end
+
+      def parsed_doc(file_data)
+        CSV.parse(file_data, headers: true)
+      end
+
+      def rewards_file_data
+        file_data(rewards_file_path)
+      end
+
+      def file_data(file_path)
+        File.read(file_path).gsub('\"', '""')
+      end
+
+      def image_for(image_name)
+        begin
+          File.open(@images_directory + '/' + image_name)
+        rescue
+          nil
+        end
+      end
+    end
+  end
+end
--- app/models/reward_template.rb
+++ app/models/reward_template.rb
@@ -1,4 +1,17 @@
+require 'dragonfly/rails/images'
 class RewardTemplate < ActiveRecord::Base
-  attr_accessible :description, :name, :price
-  attr_accessible :name, :description, :price, :as => :admin
+  include PgSearch
+
+  image_accessor :image
+
+  attr_accessible :description, :name, :price, :image, :image_uid
+  attr_accessible :name, :description, :price, :image, :image_uid, :as => :admin
+
+  scope :within_grade, lambda {|grade|  where("? BETWEEN min_grade AND max_grade", grade) }
+
+   pg_search_scope :kinda_matching,
+                   :against => [:name, :description],
+                   :using => {
+                     :tsearch => {:dictionary => "english"}
+                   }
 end
