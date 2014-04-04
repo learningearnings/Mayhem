624ecf6366aa751adfedc5dcea8e454a9da9d3e7
Adam Gamble <adamgamble@gmail.com>
2014-04-04 13:52:47 -0500

    Modified the active teacher report to take a base scope and added a for_schools scope to person

--- app/models/reports/active_teacher_report.rb
+++ app/models/reports/active_teacher_report.rb
@@ -1,22 +1,31 @@
 module Reports
   class ActiveTeacherReport
-    attr_accessor :start_date, :end_date
+    attr_accessor :start_date, :end_date, :teachers, :base_scope, :bucket
 
     def initialize options={}
       self.start_date = options.fetch(:start_date) { Time.now.beginning_of_day }
       self.end_date = options.fetch(:end_date) { Time.now.end_of_day }
+      self.base_scope = options.fetch(:base_scope) { Teacher }
     end
 
     def run
-      @teachers = Teacher.logged_in_between(start_date, end_date).includes(:otu_codes).where(OtuCode.arel_table[:created_at].gteq(start_date)).where(OtuCode.arel_table[:created_at].lteq(end_date)).uniq
+      self.bucket = base_scope
+      self.bucket = self.bucket.logged_in_between(start_date, end_date)
+      self.bucket = self.bucket.includes(:otu_codes)
+      self.bucket = self.bucket.where(OtuCode.arel_table[:created_at].gteq(start_date))
+      self.bucket = self.bucket.where(OtuCode.arel_table[:created_at].lteq(end_date))
+      self.bucket = self.bucket.uniq
+
       to_csv
     end
 
     def to_csv
       CSV.generate do |csv|
         csv << ["School Name", "Teacher First", "Teacher Last", "Teacher Email"]
-        @teachers.each do |teacher|
-          csv << [teacher.schools.first.name, teacher.first_name, teacher.last_name, teacher.user.email]
+        bucket.each_slice(100) do |teacher_slice|
+          teacher_slice.each do |teacher|
+            csv << [teacher.schools.first.name, teacher.first_name, teacher.last_name, teacher.user.email]
+          end
         end
       end
     end
