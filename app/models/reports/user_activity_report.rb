a6945608b54e6e9255a3fbe688289d740e250d61
Adam Gamble <adamgamble@gmail.com>
2014-04-09 14:50:42 -0500

    Further updated user activity report

--- app/models/reports/user_activity_report.rb
+++ app/models/reports/user_activity_report.rb
@@ -3,12 +3,14 @@ module Reports
 
     def run
       csv = CSV.generate do |csv|
-        csv << ["Teachers who logged in within the last 30 days", "% of active teachers who logged in in the last 7 days", "Teachers created within the last 7 days and logged in within the same period", "Students logged in within 30 days", "% of active students who logged in within the last 7 days", "Count of rewards purchased in the last 7 days"]
+        csv << ["Teachers logged 30 days", "active teachers logged 7", "Teachers created/loggedin within 7 days", "Students logged 30 days", "active students logged 7", "Purchases in the last 7"]
         csv_array = []
         csv_array << Teacher.recently_logged_in.count
         csv_array << ActionController::Base.helpers.number_to_percentage(BigDecimal(Teacher.logged_in_between(7.days.ago, Time.zone.now).count) / BigDecimal(Teacher.recently_logged_in.count) * 100, precision: 0)
+        csv_array << Teacher.created_between(7.days.ago, Time.zone.now).logged_in_between(7.days.ago, Time.zone.now).count
         csv_array << Student.recently_logged_in.count
         csv_array << ActionController::Base.helpers.number_to_percentage(BigDecimal(Student.logged_in_between(7.days.ago, Time.zone.now).count) / BigDecimal(Student.recently_logged_in.count) * 100, precision: 0)
+        csv_array << Student.created_between(7.days.ago, Time.zone.now).logged_in_between(7.days.ago, Time.zone.now).count
         csv_array << RewardDelivery.except_refunded.in_last_7_days.count
         csv << csv_array
       end
