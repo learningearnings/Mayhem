43d5d9caab44f6a1b72c6eef56560f05dc3671b0
Adam Gamble <adamgamble@gmail.com>
2014-04-11 09:56:06 -0500

    Ok I am shamed, fixed syntax errors in master :/

--- app/controllers/games/food_fights_controller.rb
+++ app/controllers/games/food_fights_controller.rb
@@ -1,4 +1,4 @@
-#module Games
+module Games
   class FoodFightsController < LoggedInController
     def show
       leaderboard = Games::Leaderboard.new(current_school, 'FoodFight').top(10)
@@ -39,7 +39,14 @@
       @match = FoodFightMatch.find(params[:match_id])
       @link = FoodPersonLink.create(:food_id => @food.id, :thrown_by_id => current_person.id, :person_id => @match.loser.person.id)
       @match.update_attributes(:food_person_link_id => @link.id)
-      GamesMessageStudentCommand.new(:to_id => @match.loser.person.id, :from_id => @match.winner.person.id, :body => "#{@match.winner.person.name} has thrown food at you.  <a href='/food_hit/#{@match.id}'>Click here for a rematch!</a>", :subject => 'Food Fight Match').execute! rescue nil
+      begin
+        GamesMessageStudentCommand.new(
+          :to_id => @match.loser.person.id,
+          :from_id => @match.winner.person.id,
+          :body => "#{@match.winner.person.name} has thrown food at you.  <a href='/food_hit/#{@match.id}'>Click here for a rematch!</a>", :subject => 'Food Fight Match'
+        ).execute!
+      rescue
+      end
       redirect_to games_food_fight_path, flash: { success: "Food Thrown!" }
     end
 
