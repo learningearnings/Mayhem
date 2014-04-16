4f86a1e8b8fc630c21c8408aef3c7797a59e27d2
Adam Gamble <adamgamble@gmail.com>
2014-04-16 13:19:03 -0500

    Cleaned up ajax search for reward templates

--- app/controllers/teachers/reward_templates_controller.rb
+++ app/controllers/teachers/reward_templates_controller.rb
@@ -2,25 +2,17 @@ module Teachers
   class RewardTemplatesController < Teachers::BaseController
 
     def index
-      @reward_templates = RewardTemplate.page(params[:page]).per(9)
-    end
-
-    def create
       @reward_templates = RewardTemplate
-      if params[:reward_templates_search][:search].present?
-        @reward_templates = @reward_templates.kinda_matching(params[:reward_templates_search][:search])
-      end
-      if params[:reward_templates_search][:grade_filter].present?
-        @reward_templates = @reward_templates.within_grade(params[:reward_templates_search][:grade_filter])
-      end
+      @reward_templates = @reward_templates.kinda_matching(params[:q]) if params[:q].present?
+      @reward_templates = @reward_templates.within_grade(params[:grade_filter]) if params[:grade_filter].present?
       @reward_templates = @reward_templates.page(params[:page]).per(9)
-      render :partial => "reward_templates_search_results", :locals => {:reward_templates=> @reward_templates}
+
+      render :partial => "reward_templates_search_results", :locals => {:reward_templates=> @reward_templates} if request.xhr?
     end
 
     def show
       @teachers_reward = Teachers::Reward.new
       @reward_template = RewardTemplate.find(params[:id])
     end
-
   end
 end
