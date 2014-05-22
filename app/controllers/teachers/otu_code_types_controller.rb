b84e64ba4e3f2d5609878a9cb8eb3e8c40884776
Adam Gamble <adamgamble@gmail.com>
2014-05-22 16:03:31 -0500

    Added ordering to otu code categories

--- app/controllers/teachers/otu_code_types_controller.rb
+++ app/controllers/teachers/otu_code_types_controller.rb
@@ -2,7 +2,7 @@ module Teachers
   class OtuCodeTypesController < ApplicationController
 
     def index
-      @categories = current_person.otu_code_categories.all
+      @categories = current_person.otu_code_categories.joins(:otu_code_type).order("otu_code_types.name, otu_code_categories.name").all
       @new_category = OtuCodeCategory.new
       @types = OtuCodeType.all
     end
