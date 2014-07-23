afef24938e279ad01ecff727ae09d76b1a90c5fa
David Chapman <dchapman1988@gmail.com>
2014-07-23 09:52:31 -0500

    [Parents] guid saved when linking child while logged out

--- app/controllers/parents/children_controller.rb
+++ app/controllers/parents/children_controller.rb
@@ -1,5 +1,7 @@
 module Parents
   class ChildrenController < BaseController
+    prepend_before_filter :store_guid, :only => [:link_child]
+
     def link_child
       parent_student_link = ::ParentStudentLink.where(guid: params[:guid], state: "unlinked").first
       if  parent_student_link.present?
@@ -7,7 +9,13 @@ module Parents
         parent_student_link.link!
         flash[:notice] = "You have been linked with your child."
       end
+      session[:parent_student_guid] = nil
       redirect_to main_app.parents_home_path
+    end
+
+    private
+    def store_guid
+      session[:parent_student_guid] = params[:guid]
     end
   end
 end
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -2,9 +2,9 @@ class Parent < Person
 
   has_many :parent_student_links
   has_many :students, :through => :parent_student_links
+  alias_method :children, :students
 
   def school
     schools.where("person_school_links.status = 'active'").order('created_at desc').first
   end
-
 end
