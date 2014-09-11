37051ac401f381d108243698a27d39eb3a5003c5
David Chapman <dchapman1988@gmail.com>
2014-09-11 11:47:47 -0500

    Add ability to run user activity report thats run from cron

--- app/admin/reports.rb
+++ app/admin/reports.rb
@@ -1,2 +1,12 @@
 ActiveAdmin.register_page "Reports" do
+  page_action :run_student_activity_report, :method => :get do
+    begin
+      UserActivityReportWorker.perform_async
+      flash[:notice] = 'User activity report has been started.'
+    rescue Exception => e
+      flash[:notice] = 'User activity report failed to start.'
+    ensure
+      redirect_to '/admin/reports'
+    end
+  end
 end
