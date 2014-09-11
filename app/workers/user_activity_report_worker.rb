658b19b6bea7901e74e84bf3649244d5cbd1a17e
David Chapman <dchapman1988@gmail.com>
2014-09-11 10:52:15 -0500

    fix bug with application name in background worker

--- app/workers/user_activity_report_worker.rb
+++ app/workers/user_activity_report_worker.rb
@@ -3,7 +3,7 @@ class DelayedReportWorker
 
   def perform
     require 'rake'
-    MyRailsApp::Application.load_tasks 
+    Leror::Application.load_tasks 
     Rake::Task['le:user_activity_report'].invoke
   end
 end
