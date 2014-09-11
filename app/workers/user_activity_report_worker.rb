6639e8f5160e7e7733a975be0b56ae55af038856
David Chapman <dchapman1988@gmail.com>
2014-09-11 10:49:43 -0500

    Add background worker for the user activity report

---
+++ app/workers/user_activity_report_worker.rb
@@ -0,0 +1,9 @@
+class DelayedReportWorker
+  include Sidekiq::Worker
+
+  def perform
+    require 'rake'
+    MyRailsApp::Application.load_tasks 
+    Rake::Task['le:user_activity_report'].invoke
+  end
+end
