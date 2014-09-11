1338283fd523a85f45f16d16bd46bbc9b4350383
David Chapman <dchapman1988@gmail.com>
2014-09-11 12:01:33 -0500

    render content

--- app/admin/reports.rb
+++ app/admin/reports.rb
@@ -1,4 +1,8 @@
 ActiveAdmin.register_page "Reports" do
+  content do
+    render "index"
+  end
+
   page_action :run_student_activity_report, :method => :get do
     begin
       UserActivityReportWorker.perform_async
