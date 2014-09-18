0c2e4556e8fd2fc6c79e9d4da02527ddfa1727b4
Bradley Price <bradleyprice@gmail.com>
2014-09-18 00:56:28 -0500

    - AuctionHandler doesn't take params - Don't send anything to an LeAdmin - Don't try to send any messages if we don't have   both the winner and a school_admin for validation   purposes. - No need in a message_creator method, that takes   more code to create than just calling Message.create

--- app/models/auction_handler.rb
+++ app/models/auction_handler.rb
@@ -5,43 +5,49 @@ class AuctionHandler
   include Rails.application.routes.url_helpers
   attr_accessor :auctions
 
-  def initialize params={}
-    params ||= {}
+  def initialize
     @auctions = Auction.ended.not_notified
-    @admin = LeAdmin.first
   end
 
   def run!
-    @auctions.each do |auction|
-      if auction.product.present?
-        notify_student_of_win(auction)
-        notify_admin_of_auction_end(auction)
-        auction.notify!
+    # Even though an auction has many schools, they all, from what I can tell
+    #  only have one school associated to them
+    # For the time beings, will send the message from the first school admin
+    #  and send notifications to the winner and all school admins
+    auctions.each do |auction|
+      winner = auction.current_leader
+      school_admins = auction.schools.first.school_admins
+      # If there is no school admin, we can't send the message
+      next if school_admins.blank? || auction.product.blank?
+      notify_student_of_win(auction, winner, school_admins.first)
+      school_admins.each do |admin|
+        notify_admin_of_auction_end(auction, admin, winner)
       end
-    end 
+      # auction.notify!
+    end
   end
 
   private
-  def notify_student_of_win(auction)
+  def notify_student_of_win(auction, winner, school_admin)
     body = "You've won the following auction: #{link_to(auction.to_s, Rails.application.routes.url_helpers.auction_path(auction))}."
-    message_creator.call(from: @admin,
-                         to: auction.current_leader,
-                         subject: "You've won an auction",
-                         body: body,
-                         category: 'games')
+    m = Message.create(
+      from_id: school_admin.id,
+      to_id: winner.id,
+      subject: "You've won an auction",
+      body: body,
+      category: 'games'
+    )
+    puts m.errors.full_messages
   end
 
-  def notify_admin_of_auction_end(auction)
+  def notify_admin_of_auction_end(auction, admin, winner)
     body = "The following auction has ended: #{link_to(auction.to_s, Rails.application.routes.url_helpers.auction_path(auction))}."
-    message_creator.call(from: auction.current_leader,
-                         to: @admin,
-                         subject: "An auction has ended.",
-                         body: body,
-                         category: 'le_admin')
+    Message.create(
+      from_id: winner.id,
+      to_id: admin.id,
+      subject: "An auction has ended.",
+      body: body,
+      category: 'le_admin'
+    )
   end
-
-  def message_creator
-    ->(params) { Message.create params }
-  end
-
 end
