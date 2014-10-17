7101d86dbbcd3d6ab0948cca7b1a5841b9e68302
Jimmy Benson <jimmybnsn@gmail.com>
2014-10-17 16:25:04 -0500

    adding school address as requested by STI

--- script/export_STI_customer_wise.rb
+++ script/export_STI_customer_wise.rb
@@ -1,10 +1,10 @@
 # custom export of teachers + school admins outside of Alabama to import into their CustomerWise app
-CSV.open("/Users/jimmybnsn/Downloads/LE_staff_export_customer_wise_20141015.csv", "wb") do |out_csv|
+CSV.open("/Users/jimmybnsn/Downloads/LE_staff_export_customer_wise_#{Date.today.to_s}.csv", "wb") do |out_csv|
   out_csv << ["School","City","State","First","Last","Email","Type","Synced?"]
   Teacher.recently_logged_in.each do |staff|
     staff.sti_id ? synced = "yes" : synced = "no"
     unless staff.school.state.id == 1 
-      out_csv << ["#{staff.school.name}","#{staff.school.city}","#{staff.school.state.abbr}","#{staff.first_name}","#{staff.last_name}","#{staff.user.email}","#{staff.type}","#{synced}"] 
+      out_csv << ["#{staff.school.name}","#{staff.school.address1}","#{staff.school.city}","#{staff.school.state.abbr}","#{staff.first_name}","#{staff.last_name}","#{staff.user.email}","#{staff.type}","#{synced}"] 
     end
   end  
 end
