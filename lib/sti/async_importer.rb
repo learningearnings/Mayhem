f0704da56e8054c6f29317792b1b949185bff80d
Bradley Price <bradleyprice@gmail.com>
2014-10-24 14:13:00 -0500

    Fix some issues with synchronizers calling incorrect values and fix mass assignment issues with district model.

--- lib/sti/async_importer.rb
+++ lib/sti/async_importer.rb
@@ -1,15 +1,15 @@
 # This class should be called like so:
 #   sti_link_token = StiLinkToken.first
 #   client = STI::Client.new(base_url: sti_link_token.api_url, username: sti_link_token.username, password: sti_link_token.password)
-#   STI::AsyncImporter.new(client: client, district_guid: district_guid).execute!
+#   STI::AsyncImporter.new(client: client, district_guid: sti_link_token.district_guid).run!
 
-require 'lib/sti/client.rb'
-require 'lib/sti/synchronizers/base_synchronizer.rb'
-require 'lib/sti/synchronizers/staff_synchronizer.rb'
-require 'lib/sti/synchronizers/roster_synchronizer.rb'
-require 'lib/sti/synchronizers/school_synchronizer.rb'
-require 'lib/sti/synchronizers/student_synchronizer.rb'
-require 'lib/sti/synchronizers/classroom_synchronizer.rb'
+load 'lib/sti/client.rb'
+load 'lib/sti/synchronizers/base_synchronizer.rb'
+load 'lib/sti/synchronizers/staff_synchronizer.rb'
+load 'lib/sti/synchronizers/roster_synchronizer.rb'
+load 'lib/sti/synchronizers/school_synchronizer.rb'
+load 'lib/sti/synchronizers/student_synchronizer.rb'
+load 'lib/sti/synchronizers/classroom_synchronizer.rb'
 
 module STI
   class AsyncImporter
@@ -19,32 +19,36 @@ module STI
     end
 
     def run!
-      district = District.where(district_guid: @district_guid).first
-
+      district = District.where(guid: @district_guid).first
 
       ##### Update Schools ######
       sti_schools = client.schools.parsed_response
-      school_synchronizer = STI::Synchronizers::SchoolSynchronizer.new(sti_schools, @district_guid)
+      puts sti_schools
+      school_synchronizer = STI::Synchronizers::SchoolSynchronizer.new(sti_schools || [], @district_guid)
       school_synchronizer.execute!
 
       ##### Update Staff #####
       staff_response = client.async_staff(district.current_staff_version).parsed_response
-      staff_synchronizer = STI::Synchronizers::StaffSynchronizer.new(staff_response, @district_guid)
+      puts staff_response
+      staff_synchronizer = STI::Synchronizers::StaffSynchronizer.new(staff_response || [], @district_guid)
       staff_synchronizer.execute!
 
       ##### Update Classrooms #####
       section_response = client.async_sections(district.current_section_version).parsed_response
-      classroom_synchronizer = STI::Synchronizers::ClassroomSynchronizer.new(section_response, @district_guid)
+      puts section_response
+      classroom_synchronizer = STI::Synchronizers::ClassroomSynchronizer.new(section_response || [], @district_guid)
       classroom_synchronizer.execute!
 
       ##### Update Students #####
       student_response = client.async_students(district.current_student_version).parsed_response
-      student_synchronizer = STI::Synchronizers::StudentSynchronizer.new(student_response, @district_guid)
+      puts student_response
+      student_synchronizer = STI::Synchronizers::StudentSynchronizer.new(student_response || [], @district_guid)
       student_synchronizer.execute!
 
       ##### Update Rosters #####
       roster_response = client.async_rosters(district.current_roster_version).parsed_response
-      roster_synchronizer = STI::Synchronizers::RosterSynchronizer.new(roster_response, @district_guid)
+      puts roster_response
+      roster_synchronizer = STI::Synchronizers::RosterSynchronizer.new(roster_response || [], @district_guid)
       roster_synchronizer.execute!
 
       ##### Update API #####
