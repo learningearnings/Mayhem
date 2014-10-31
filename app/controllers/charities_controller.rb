4c2685b0c778a7eb9541bd7a4253c5c53f6c71d6
Bradley Price <bradleyprice@gmail.com>
2014-10-31 15:40:07 -0500

    First stab at moving available charities to charities section.
    Restyled existing "Your charities" section to make
    all of the charities look like the product tiles
    that are being used in other spots.

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,7 +1,8 @@
 class CharitiesController < LoggedInController
   def index
-    charities = Plutus::Transaction.for_person(current_person.id).with_main_account.with_spree_products.merge(Spree::Product.with_property_value('reward_type','charity')).order('plutus_transactions.created_at desc').page(params[:page]).per(4)
-    render 'index', locals: { charities: charities }
+    purchased_charities = Plutus::Transaction.for_person(current_person.id).with_main_account.with_spree_products.merge(Spree::Product.with_property_value('reward_type','charity')).order('plutus_transactions.created_at desc').page(params[:purchased_charities_page]).per(3)
+    available_charities = Spree::Product.active.charities.page(params[:available_charities_page]).per(3)
+    render 'index', locals: { purchased_charities: purchased_charities, available_charities: available_charities }
   end
   def print
     charities = Plutus::Transaction.where(:id => params[:id]).with_spree_products.merge(Spree::Product.with_property_value('reward_type','charity'))
