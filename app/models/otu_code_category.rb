7d0dbfe6a6d2dc3697b4d0a6f76bb06103ac2596
Bradley Price <bradleyprice@gmail.com>
2014-11-14 18:09:00 -0600

    add school_wide? method to otu_code_category and fix typo

--- app/models/otu_code_category.rb
+++ app/models/otu_code_category.rb
@@ -9,4 +9,8 @@ class OtuCodeCategory < ActiveRecord::Base
 
   default_scope { joins(:otu_code_type).order("otu_code_types.name, otu_code_categories.name") }
 
+  def school_wide?
+    school_id.present?
+  end
+
 end
