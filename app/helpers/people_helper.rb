0cc95c0358148ba3a1c376b08e67455c707eb72a
Allen <allen@allen-le.(none)>
2014-11-19 16:38:27 -0600

    Updates to students checking ledget

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,5 @@
 module PeopleHelper
+  def source_from_transaction(transaction)
+    (transaction.spree_product)?(transaction.spree_product.person.full_name) : ((transaction.people.size > 0)?(transaction.people[0].full_name):"none")
+  end
 end
