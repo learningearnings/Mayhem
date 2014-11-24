ecfa2283c9769ac7f43b235d914f1ff8c7dd1273
Allen <allen@allen-le.(none)>
2014-11-24 08:57:47 -0600

    reduced function in people_helper

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,11 +1,5 @@
 module PeopleHelper
   def source_from_transaction(transaction)
-    if transaction.spree_product.try(:person)
-      transaction.spree_product.person.full_name
-    elsif (transaction.people.size > 0)
-      transaction.people[0].full_name
-    else
-      "none"
-    end
+    transaction.spree_product.try(:person).try(:full_name) || transaction.people.first.try(:full_name) || "none"
   end
 end
