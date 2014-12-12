db876616f8fa09435bce35f3101467a82dfbddf6
Allen Taylor <akt@frikshun.com>
2014-12-11 19:44:51 -0500

    Update orders_controller_decorator.rb
    Mutex key includes product id

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -29,8 +29,12 @@ Spree::OrdersController.class_eval do
 
 
   def populate
-    #If another student is purchasing this reward, wait 10 seconds and then return an error if the reward has not freed up
-    mutex = RedisMutex.new('orders-blocking', block: 1)
+    variant_options = params[:variants].to_a.flatten
+    variant  = Spree::Variant.find variant_options.first
+
+    #If another student is purchasing this reward, wait 10 seconds and then return an error if the reward has not freed up    
+    mutex = RedisMutex.new("orders-blocking-#{variant.product_id}", block: 10)
+
     if mutex.lock
       begin
         if insufficient_quantity?
