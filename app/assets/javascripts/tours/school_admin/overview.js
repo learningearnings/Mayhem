30b572b40fec2866ea9b1cbf0c9e5b1a22ecd69b
Bradley Price <bradleyprice@gmail.com>
2014-12-29 14:07:51 -0600

    Changes to tour, moving steps around per Jimmy.

--- app/assets/javascripts/tours/school_admin/overview.js
+++ app/assets/javascripts/tours/school_admin/overview.js
@@ -6,6 +6,43 @@ var school_admin_overview = {
     placement: 'bottom',
     multipage: true,
     onNext: function() {
+      window.location = "/classrooms"
+    }
+  }, {
+    target: document.querySelector('.classrooms'),
+    content: "The classrooms page allows you to create groups of students. Simply enter a name for the classroom, click create, then click the classroom name.",
+    placement: "bottom"
+  }, {
+    target: document.querySelector('.new_classroom'),
+    content: "To start, simply type a classroom name and click Create.",
+    placement: "bottom",
+    showNextButton: false
+  }, {
+    target: document.querySelector('#classrooms'),
+    content: "Now that you've created your classroom, click the classroom to add a student.",
+    placement: "left",
+    showNextButton: false,
+    multipage: true
+  }, {
+    target: document.querySelector('.resp-page-content'),
+    content: "This is where you add students to your classroom",
+    placement: "top"
+  }, {
+    target: document.querySelector('.btn.add-new-student'),
+    content: "Click here to add a new student",
+    placement: "top",
+    showNextButton: false
+  }, {
+    target: document.querySelector('#add-new-student-modal'),
+    content: "This is where you will add a new student's information. Enter your student's information and click Add",
+    placement: "left",
+    showNextButton: false
+  }, {
+    target: document.querySelector('#classroom-students'),
+    content: "The new user shows up in the table.",
+    placement: "top",
+    multipage: true,
+    onNext: function() {
       window.location = "/school_admins/bank"
     }
   }, {
@@ -15,54 +52,58 @@ var school_admin_overview = {
   }, {
     target: $('a[href=#auto-credits]')[0],
     content: "This tab allows you to set parameters for iNow to automatically send electronic credits to your students.",
-    placement: "left",
-    multipage: true,
-    onNext: function() {
-      window.location = "/teachers/bulk_students"
-    }
-  }, {
-    target: document.querySelector('.manage_students'),
-    content: "The Students page is where you can edit your students accounts and reset passwords if necessary.",
-    placement: "bottom"
-  }, {
-    target: document.querySelector('.new-students-link'),
-    content: "The Students page is where you can edit your students accounts and reset passwords if necessary.",
-    placement: "bottom",
-    multipage: true,
-    onNext: function() {
-      window.location = "/classrooms"
-    }
-  }, {
-    target: document.querySelector('.classrooms'),
-    content: "The classrooms page allows you to create groups of students. Simply enter a name for the classroom, click create, then click the classroom name.",
-    placement: "bottom"
-  }, {
-    target: document.querySelector('.new_classroom'),
-    content: "Classrooms allow you to give your students LE credits quickly. If you have classrooms in iNow, they should come over automatically, but you can still create more if you like.",
-    placement: "bottom"
-  }, {
-    target: $('.classroom')[0], //document.querySelector('.classroom')[0],
-    content: "You can't edit Classrooms that are synced with iNow, but any additional Classrooms you create can be customized by you.",
-    placement: "bottom",
-    multipage: true,
-    onNext: function() {
-      window.location = "/store"
-    }
-  }, {
-    target: document.querySelector('.shop'),
-    content: "The Store is where you create and manage prizes or rewards for your classrooms. This page will show you all the rewards in your school.",
-    placement: "bottom"
-  }, {
-    target: document.querySelector('.manage-rewards'),
-    content: "You can add a new reward or update and existing one here.",
-    placement: "bottom",
-    multipage: true,
-    onNext: function() {
-      window.location = "/school_admins/reports"
-    }
-  }, {
-    target: document.querySelector('.reports'),
-    content: "Run a Purchase Report to see what items your students have purchased from the store. You can also check student Credit balances or see who's logging in.",
-    placement: "bottom"
+    placement: "left"
+    // multipage: true,
+    // onNext: function() {
+    //   window.location = "/teachers/bulk_students"
+    // }
   }]
 }
+
+// }, {
+//   target: document.querySelector('.manage_students'),
+//   content: "The Students page is where you can edit your students accounts and reset passwords if necessary.",
+//   placement: "bottom"
+// }, {
+//   target: document.querySelector('.new-students-link'),
+//   content: "The Students page is where you can edit your students accounts and reset passwords if necessary.",
+//   placement: "bottom",
+//   multipage: true,
+//   onNext: function() {
+//     window.location = "/classrooms"
+//   }
+// }, {
+//   target: document.querySelector('.classrooms'),
+//   content: "The classrooms page allows you to create groups of students. Simply enter a name for the classroom, click create, then click the classroom name.",
+//   placement: "bottom"
+// }, {
+//   target: document.querySelector('.new_classroom'),
+//   content: "Classrooms allow you to give your students LE credits quickly. If you have classrooms in iNow, they should come over automatically, but you can still create more if you like.",
+//   placement: "bottom"
+// }, {
+
+// , {
+//   target: $('.classroom')[0], //document.querySelector('.classroom')[0],
+//   content: "You can't edit Classrooms that are synced with iNow, but any additional Classrooms you create can be customized by you.",
+//   placement: "bottom",
+//   multipage: true,
+//   onNext: function() {
+//     window.location = "/store"
+//   }
+// }, {
+//   target: document.querySelector('.shop'),
+//   content: "The Store is where you create and manage prizes or rewards for your classrooms. This page will show you all the rewards in your school.",
+//   placement: "bottom"
+// }, {
+//   target: document.querySelector('.manage-rewards'),
+//   content: "You can add a new reward or update and existing one here.",
+//   placement: "bottom",
+//   multipage: true,
+//   onNext: function() {
+//     window.location = "/school_admins/reports"
+//   }
+// }, {
+//   target: document.querySelector('.reports'),
+//   content: "Run a Purchase Report to see what items your students have purchased from the store. You can also check student Credit balances or see who's logging in.",
+//   placement: "bottom"
+// }
