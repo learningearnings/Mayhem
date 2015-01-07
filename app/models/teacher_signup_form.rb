74b9e2db01d1179d3878f10c135441c0e092b536
Bradley Price <bradleyprice@gmail.com>
2015-01-07 09:02:16 -0600

    Add back Allen's email validator that got removed

--- app/models/teacher_signup_form.rb
+++ app/models/teacher_signup_form.rb
@@ -10,7 +10,7 @@ class TeacherSignupForm
 
   validates :first_name,            presence: true
   validates :last_name,             presence: true
-  validates :email,                 presence: true
+  validates :email,                 presence: true, email: true
   validates :grade,                 presence: true
   validates :username,              presence: true
   validates :password,              presence: true
