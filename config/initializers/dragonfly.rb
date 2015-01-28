f0686a5c28267a89c339e645ae8869e4a75aea5a
Allen Taylor <aktaylor@sti-k12.com>
2015-01-28 15:49:00 -0600

    Restore Dragonfly settings

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,2 +1,13 @@
 require 'dragonfly/rails/images'
 app = Dragonfly[:images]
+app.configure do |c|
+  c.allow_fetch_file = true
+  c.allow_fetch_url = true
+end
+app.datastore = Dragonfly::DataStorage::S3DataStore.new({
+  bucket_name: ENV['LE_S3_BUCKET'],
+  access_key_id: ENV['LE_S3_ACCESS_KEY'],
+  secret_access_key: ENV['LE_S3_SECRET_ACCESS_KEY'],
+  region: 'us-west-2',
+  url_scheme: 'https'
+})
