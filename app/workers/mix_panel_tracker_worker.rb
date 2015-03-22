dd6963fba186d4e2b1839143f1c33258eee8f13f
Allen Taylor <aktaylor@sti-k12.com>
2015-03-22 09:38:23 -0500

    Only send tracker message if in production env

--- app/workers/mix_panel_tracker_worker.rb
+++ app/workers/mix_panel_tracker_worker.rb
@@ -3,16 +3,7 @@ class MixPanelTrackerWorker
   @token = "6980dec826990c22d5bbef3a690bd599"
 
   def perform(user_id, event_name, options={})
-    Mixpanel::Tracker.new(@token).track(user_id, event_name, options)
+    Mixpanel::Tracker.new(@token).track(user_id, event_name, options) if Rails.env.production?
   end
 
-  def identify user_id, options={}
-    tracker = Mixpanel::Tracker.new(@token)
-    tracker.people.set(user_id, options)
-  end
-  
-  #def track user_id, event_name, options={}
-  #  tracker = Mixpanel::Tracker.new(@token)
-  #  tracker.track(user_id, event_name, options)
-  #end
 end
