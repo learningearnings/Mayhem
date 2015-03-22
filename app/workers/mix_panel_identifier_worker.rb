15049c4bfe66632bd0660de6b61b9e2c88cd67c5
Allen Taylor <aktaylor@sti-k12.com>
2015-03-22 10:25:20 -0500

    Moved token

--- app/workers/mix_panel_identifier_worker.rb
+++ app/workers/mix_panel_identifier_worker.rb
@@ -1,8 +1,7 @@
 class MixPanelIdentifierWorker
   include Sidekiq::Worker
-  @@token = "6980dec826990c22d5bbef3a690bd599"
 
   def perform(user_id, options={})
-    Mixpanel::Tracker.new(@@token).people.set(user_id, options)
+    Mixpanel::Tracker.new("6980dec826990c22d5bbef3a690bd599").people.set(user_id, options)
   end
 end
--- app/workers/mix_panel_tracker_worker.rb
+++ app/workers/mix_panel_tracker_worker.rb
@@ -1,9 +1,8 @@
 class MixPanelTrackerWorker
   include Sidekiq::Worker
-  @token = "6980dec826990c22d5bbef3a690bd599"
 
   def perform(user_id, event_name, options={})
-    Mixpanel::Tracker.new(@token).track(user_id, event_name, options) if Rails.env.production?
+    Mixpanel::Tracker.new("6980dec826990c22d5bbef3a690bd599").track(user_id, event_name, options) if Rails.env.production?
   end
 
 end
