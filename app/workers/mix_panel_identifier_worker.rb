a1a065fe7d1893b6309bd5d4e77dc951b1123d24
Allen Taylor <aktaylor@sti-k12.com>
2015-03-23 07:13:01 -0500

    Only track in production

--- app/workers/mix_panel_identifier_worker.rb
+++ app/workers/mix_panel_identifier_worker.rb
@@ -2,6 +2,6 @@ class MixPanelIdentifierWorker
   include Sidekiq::Worker
 
   def perform(user_id, options={})
-    Mixpanel::Tracker.new("6980dec826990c22d5bbef3a690bd599").people.set(user_id.to_s, options) 
+    Mixpanel::Tracker.new("6980dec826990c22d5bbef3a690bd599").people.set(user_id.to_s, options)  if Rails.env.production?
   end
 end
