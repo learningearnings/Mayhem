8d1d2867da3a812b262985d3599f908fa782b424
Allen Taylor <akt@frikshun.com>
2015-03-30 12:41:16 -0500

    Added route for partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 Leror::Application.routes.draw do
 
-
+  get '/homes/schools_for_username' => "homes#schools_for_username"
   get '/sti/give_credits' => "sti#give_credits"
   get '/sti/new_school_for_credits' => "sti#new_school_for_credits"  
   post '/sti/save_school_for_credits' => "sti#save_school_for_credits"    
