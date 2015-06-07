7cfef568281ee4e797cf55b3c81a834ad58b6b3c
Allen Taylor <aktaylor@sti-k12.com>
2015-06-07 07:00:37 -0500

    Mailer

--- app/models/reports/alsde_study/students_report.rb
+++ app/models/reports/alsde_study/students_report.rb
@@ -16,19 +16,21 @@ module Reports
         CSV.generate do |csv|
           csv << ["sti_district_guid", "sti_school_id", "sti_user_id", "le_person_id", "grade", "status", "first_login_date", "login_count", "sum_credits_deposited", "sum_credits_spent_on_purchases"]
           students.each do |student|
-            csv << [
-              student.district_guid,
-              student.school.try(:sti_id),
-              student.sti_id,
-              student.id,
-              student.grade,
-              student.status,
-              student.interactions.first.try(:created_at).try(:strftime, "%m/%d/%Y"),
-              student.user.sign_in_count,
-              # TODO: Make sure this is right
-              student.otu_codes.redeemed_between(@start_date, @end_date).sum(:points).to_s,
-              sum_credits_spent_on_purchases_for(student)
-            ]
+            if student.user
+              csv << [
+                student.district_guid,
+                student.school.try(:sti_id),
+                student.sti_id,
+                student.id,
+                student.grade,
+                student.status,
+                student.interactions.first.try(:created_at).try(:strftime, "%m/%d/%Y"),
+                student.user.sign_in_count,
+                # TODO: Make sure this is right
+                student.otu_codes.redeemed_between(@start_date, @end_date).sum(:points).to_s,
+                sum_credits_spent_on_purchases_for(student)
+              ]
+            end
           end
         end
       end
