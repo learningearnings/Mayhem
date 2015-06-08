280de115a58a078115d0dab68df46cb1791d163b
Allen Taylor <aktaylor@sti-k12.com>
2015-06-08 16:33:50 -0500

    Optimize student/staff report

--- app/models/reports/alsde_study/staff_report.rb
+++ app/models/reports/alsde_study/staff_report.rb
@@ -11,26 +11,34 @@ module Reports
       end
 
       def run
+        @begin_time = Time.now
+
         school_ids =  School.where(district_guid: District.where(alsde_study: true).pluck(:guid)).pluck(:id)
-        staff = Teacher.joins(:allperson_school_links).where(allperson_school_links: { school_id: school_ids })
+        staff = Teacher.includes(:user).joins(:allperson_school_links).where(status: 'active', allperson_school_links: { school_id: school_ids })
+        puts "Looping through #{staff.size} teachers..."
         CSV.generate do |csv|
           csv << ["sti_district_guid", "sti_school_id", "sti_user_id", "le_person_id", "status", "first_login_date", "login_count", "sum_credits_awarded", "has_a_classroom?", "grade"]
-          staff.each do |staff_member|
-            csv << [
-              staff_member.district_guid,
-              # TODO: This might be incorrect if a staff_member belongs to multiple schools
-              staff_member.school.try(:sti_id),
-              staff_member.sti_id,
-              staff_member.id,
-              staff_member.status,
-              staff_member.interactions.first.try(:created_at).try(:strftime, "%m/%d/%Y"),
-              staff_member.user.sign_in_count,
-              # TODO: Make sure this is right
-              staff_member.otu_codes.created_between(@start_date, @end_date).sum(:points).to_s,
-              staff_member.classrooms.any?,
-              staff_member.grade
-            ]
+          staff.each_with_index do |staff_member, index|
+            if staff_member.user            
+              csv << [
+                  staff_member.district_guid,
+                  # TODO: This might be incorrect if a staff_member belongs to multiple schools
+                  staff_member.school.try(:sti_id),
+                  staff_member.sti_id,
+                  staff_member.id,
+                  staff_member.status,
+                  staff_member.interactions.first.try(:created_at).try(:strftime, "%m/%d/%Y"),
+                  staff_member.user.sign_in_count,
+                  # TODO: Make sure this is right
+                  staff_member.otu_codes.created_between(@start_date, @end_date).sum(:points).to_s,
+                  staff_member.classrooms.any?,
+                  staff_member.grade
+              ]
+              puts "Processing teacher #{index} of #{staff.size}"
+            end
           end
+         puts "Staff Report: Begin at: #{@begin_time}"          
+         puts "Staff Report: Ends at: #{Time.now}"
         end
       end
     end
