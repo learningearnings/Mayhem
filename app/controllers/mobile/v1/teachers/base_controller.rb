9fd3c95ba6de58883c8532e7a2310f34671203c9
Allen Taylor <aktaylor@sti-k12.com>
2015-06-19 09:57:57 -0500

    Push

--- app/controllers/mobile/v1/teachers/base_controller.rb
+++ app/controllers/mobile/v1/teachers/base_controller.rb
@@ -1,12 +1,12 @@
 class Mobile::V1::Teachers::BaseController < Mobile::V1::BaseController
   def authenticate
-    Rails.logger.warn("AKT Teachers Mobile authenticate")
+    #Rails.logger.warn("AKT Teachers Mobile authenticate")
     user = Spree::User.joins(:person).where('people.type IN (?)', ['Teacher', 'SchoolAdmin']).authenticate_with_school_id(params[:username], params[:password], params[:school_id])
     if user
-      Rails.logger.warn("Mobile authenticate success, user: #{user.inspect}")
+      #Rails.logger.warn("Mobile authenticate success, user: #{user.inspect}")
       render json: { auth_token: user.generate_auth_token_with_school_id(params[:school_id]), user: user }
     else
-      Rails.logger.warn("Mobile authenticate failure, user: #{params.inspect}")      
+      #Rails.logger.warn("Mobile authenticate failure, user: #{params.inspect}")      
       render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
   end
