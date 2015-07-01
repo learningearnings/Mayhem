312ab2990a2162395da612748cc476907b53d614
Allen Taylor <aktaylor@sti-k12.com>
2015-07-01 16:06:21 -0500

    Add mobile auth messages

--- app/controllers/mobile/v1/teachers/base_controller.rb
+++ app/controllers/mobile/v1/teachers/base_controller.rb
@@ -1,13 +1,19 @@
 class Mobile::V1::Teachers::BaseController < Mobile::V1::BaseController
   def authenticate
-    #Rails.logger.warn("AKT Teachers Mobile authenticate")
+    if params[:username].blank?
+      render json: { error: 'Please enter a username' }, status: :unauthorized and return
+    end
+    if params[:password].blank?
+      render json: { error: 'Please enter a password' }, status: :unauthorized and return
+    end   
+    if params[:school_id].blank?
+      render json: { error: 'Please select your school' }, status: :unauthorized and return
+    end    
     user = Spree::User.joins(:person).where('people.type IN (?)', ['Teacher', 'SchoolAdmin']).authenticate_with_school_id(params[:username], params[:password], params[:school_id])
     if user
-      #Rails.logger.warn("Mobile authenticate success, user: #{user.inspect}")
       render json: { auth_token: user.generate_auth_token_with_school_id(params[:school_id]), user: user }
-    else
-      #Rails.logger.warn("Mobile authenticate failure, user: #{params.inspect}")      
-      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    else     
+      render json: { error: 'Invalid username, password or school selection' }, status: :unauthorized
     end
   end
 end
