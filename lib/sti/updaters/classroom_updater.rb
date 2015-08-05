d781f8fd4f54746382afb3851e9662f0cb4e8aa3
Allen Taylor <aktaylor@sti-k12.com>
2015-08-05 09:13:05 -0500

    Add check for person_school_link in classroom updater

--- lib/sti/updaters/classroom_updater.rb
+++ lib/sti/updaters/classroom_updater.rb
@@ -14,7 +14,7 @@ module STI
 
         teacher = Teacher.where(:district_guid => @district_guid, :sti_id => @data["TeacherId"]).first
         person_school_link = teacher.person_school_links.includes(:school).where("schools.district_guid" => @district_guid, "schools.sti_id" => @data["SchoolId"]).first
-        PersonSchoolClassroomLink.where(:person_school_link_id => person_school_link.id, :classroom_id => classroom.id).first_or_create
+        PersonSchoolClassroomLink.where(:person_school_link_id => person_school_link.id, :classroom_id => classroom.id).first_or_create if person_school_link and classroom
       end
 
       private
