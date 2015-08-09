fe4f6aad15d23c9e736a54ee87c99a1d5513e33e
Allen Taylor <aktaylor@sti-k12.com>
2015-08-09 18:27:40 -0500

    Checks for missing api data

--- lib/sti/synchronizers/staff_synchronizer.rb
+++ lib/sti/synchronizers/staff_synchronizer.rb
@@ -5,9 +5,9 @@ module STI
   module Synchronizers
     class StaffSynchronizer < BaseSynchronizer
       def execute!
-        deleted.each { |member| STI::Deactivators::StaffDeactivator.new(member, @district_guid).execute! }
-        inserted.each{ |member| STI::Creators::StaffCreator.new(member, @district_guid).execute! }
-        updated.each { |member| STI::Updaters::StaffUpdater.new(member, @district_guid).execute! }
+        deleted.each { |member| STI::Deactivators::StaffDeactivator.new(member, @district_guid).execute! } if deleted
+        inserted.each{ |member| STI::Creators::StaffCreator.new(member, @district_guid).execute! } if inserted
+        updated.each { |member| STI::Updaters::StaffUpdater.new(member, @district_guid).execute! } if updated
         update_current_version(:current_staff_version)
       end
     end
