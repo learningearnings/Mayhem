1460d431f6d4e39ba10e3f1fc334d1a1bf5f016e
Allen Taylor <aktaylor@sti-k12.com>
2015-08-21 09:55:49 -0500

    Add POST for STI#auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require 'sidekiq/web'
 Leror::Application.routes.draw do
+  post '/sti/auth' => "sti#auth"   
   get '/sti/auth' => "sti#auth" 
   get '/homes/schools_for_username' => "homes#schools_for_username"
   get  '/teachers/home/defer_email' => "teachers/home#defer_email"
