a208ec817f62e827d6454b592461b438b48d1d8a
Jimmy Benson <jimmybnsn@gmail.com>
2015-09-10 16:03:28 -0500

    updating sandbox enviro to use the sandbox branch and adding a sidekiq role

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,6 +1,7 @@
 set :rails_env, "sandbox"
-set :branch,  "sti_beta"
+set :branch,  "sandbox"
 set :unicorn_rack_env, "sandbox"
 role :web,    "162.243.39.36"
 role :app,    "162.243.39.36"
 role :db,     "162.243.39.36", primary: true
+role :sidekiq, "162.243.202.62"
