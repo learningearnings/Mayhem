83697192c5117269b7a297d06af6f16e493d12e8
Allen Taylor <aktaylor@sti-k12.com>
2015-09-28 13:00:16 -0500

    update creators

--- lib/sti/creators/classroom_creator.rb
+++ lib/sti/creators/classroom_creator.rb
@@ -16,8 +16,10 @@ module STI
         teacher = Teacher.where(:district_guid => @district_guid, :sti_id => @data["TeacherId"]).first
         if teacher
           Rails.logger.debug "AKT: Fetch PersonSchoolLink for district_guid #{@district_guid} and school sti_id #{@data["SchoolId"]}"
-          person_school_link = teacher.person_school_links.includes(:school).where("schools.district_guid" => @district_guid, "schools.sti_id" => @data["SchoolId"]).first
-          Rails.logger.debug "AKT: #{person_school_link.inspect}"
+          school = School.where(district_guid: @district_guid, sti_id: @data["SchoolId"], status: "active")
+          Rails.logger.debug "AKT: School: #{school}"
+          person_school_link = PersonSchoolLink.where(person_id: teacher.id, school_id: school.id, status: "active").first_or_create
+          Rails.logger.debug "AKT: PSL #{person_school_link.inspect}"
           PersonSchoolClassroomLink.where(:person_school_link_id => person_school_link.id, :classroom_id => classroom.id).first_or_create
         end
     end
