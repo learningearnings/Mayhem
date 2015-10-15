3826cfdafd0b8dc06c0a4e8cb5980c2475181baf
Allen Taylor <aktaylor@sti-k12.com>
2015-10-15 14:11:16 -0500

    Merge in staging updates

--- app/models/auction_creator.rb
+++ app/models/auction_creator.rb
@@ -3,6 +3,7 @@ class AuctionCreator
 
   def initialize(params = nil, current_person)
     @params = params
+    Rails.logger.debug("AKT : params #{@params.inspect}")
     @current_person = current_person
   end
 
@@ -12,7 +13,11 @@ class AuctionCreator
     auction.start_date = Time.zone.parse(@params[:start_date]) if @params[:start_date].present?
     auction.end_date = Time.zone.parse(@params[:end_date]) if @params[:end_date].present?
     auction.set_local if @current_person.is_a?(SchoolAdmin)
-    create_school_links if auction.save
+    if auction.save
+      create_school_links 
+      create_auction_zip_codes
+      create_state_links
+    end
   end
 
   def created?
@@ -21,7 +26,26 @@ class AuctionCreator
 
   private
   def create_school_links
-    # TODO: Replace this with parameter being passed in
-    AuctionSchoolLink.create(:school_id => @current_person.school.id, :auction_id => auction.id)
+    school_ids = @params[:school_ids]
+    Rails.logger.debug("AKT: Create School Links: #{school_ids.inspect}")
+    school_ids.each do | school_id |
+      school = School.where(id: school_id).first
+      AuctionSchoolLink.create(:school_id => school.id, :auction_id => auction.id) if school
+    end
+  end
+  def create_auction_zip_codes
+    if @params[:auction_zip_code_ids]
+      @params[:auction_zip_code_ids].each do |zip|
+        zip = zip.strip
+        AuctionZipCode.create(:zip_code => zip, :auction_id => auction.id) if zip.present?
+      end
+    end
+  end  
+  def create_state_links
+    if @params[:state_ids]
+      @params[:state_ids].each do |state|
+        AuctionStateLink.create(:state_id => state, :auction_id => auction.id) if state.present?
+      end    
+    end
   end
 end
