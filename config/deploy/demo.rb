6fbf7d92e296957414091a903d4b88c53f5a131c
Allen Taylor <aktaylor@sti-k12.com>
2016-02-25 13:52:15 -0600

    Set up demo config

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,6 +1,7 @@
 set :rails_env, "demo"
-set :branch,  "sti_beta"
+set :branch,  "demo"
 set :unicorn_rack_env, "demo"
 role :web,    "162.243.9.20"
 role :app,    "162.243.9.20"
 role :db,     "162.243.9.20", primary: true
+role :sidekiq, "162.243.9.20"
