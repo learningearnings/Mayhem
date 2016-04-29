6eedaa438c500205652926bf95ea68ed2da17c20
Sonam Shah <sonam.shah@clariontechnologies.co.in>
2016-04-29 06:01:51 -0500

    Added Homeroom to Student Activity report.

--- app/models/reports/activity.rb
+++ app/models/reports/activity.rb
@@ -85,6 +85,8 @@ module Reports
       Reports::Row[
         person: person.name,
         username: person.person_username,
+        #classroom: person.classrooms_for_school(@school).map(&:name).join(","),
+        homeroom: person.homeroom,
         account_activity: (number_with_precision(person.activity_balance, precision: 2, delimiter: ',') || 0),
         type: person.type,
         last_sign_in_at: (person.last_sign_in_at)?time_ago_in_words(person.last_sign_in_at) + " ago":""
@@ -95,6 +97,7 @@ module Reports
       {
         person: "Person",
         username: "Username",
+        homeroom: "Homeroom",
         type: "Type",
         last_sign_in_at: "Last Sign In",
         account_activity: "Account Balance"
@@ -104,6 +107,7 @@ module Reports
       {
         person: "",
         username: "",
+        homeroom: "",
         type: "",
         last_sign_in_at: "",
         account_activity: "currency"
