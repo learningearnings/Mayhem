a46ed16295e1ab7a4dc10ccda43937200bba2190
Sonam Shah <sonam.shah@clariontechnologies.co.in>
2016-04-29 05:46:48 -0500

    Commented current school classroom for now and added homeroom till we get confirmation.

--- app/models/reports/activity.rb
+++ app/models/reports/activity.rb
@@ -85,8 +85,8 @@ module Reports
       Reports::Row[
         person: person.name,
         username: person.person_username,
-        classroom: person.classrooms_for_school(@school).map(&:name).join(","),
-        #classroom: person.homeroom,
+        #classroom: person.classrooms_for_school(@school).map(&:name).join(","),
+        classroom: person.homeroom,
         account_activity: (number_with_precision(person.activity_balance, precision: 2, delimiter: ',') || 0),
         type: person.type,
         last_sign_in_at: (person.last_sign_in_at)?time_ago_in_words(person.last_sign_in_at) + " ago":""
