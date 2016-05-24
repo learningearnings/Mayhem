9142b1629978e620c658abc93118acb972a8f15f
Sonam Shah <sonam.shah@clariontechnologies.co.in>
2016-05-24 08:16:35 -0500

    Added instance object for student.

--- app/controllers/reports/student_earning_controller.rb
+++ app/controllers/reports/student_earning_controller.rb
@@ -9,7 +9,7 @@ module Reports
       }
     end
     def credit_transactions      
-      student = Student.find(params[:student_id])
+      @student = Student.find(params[:student_id])
       start_date = DateTime.parse(params[:start_date])
       end_date = DateTime.parse(params[:end_date])
       @credit_type = params[:credit_type] 
@@ -20,7 +20,7 @@ module Reports
         @otu_codes = @otu_codes.active
       end 
       respond_to do |format|
-        format.html { render partial: 'credit_transactions', layout: false,  locals: { otu_codes: @otu_codes, credit_type: @credit_type, student_name: student.name } }
+        format.html { render partial: 'credit_transactions', layout: false,  locals: { otu_codes: @otu_codes, credit_type: @credit_type, student_name: @student.name } }
         format.js 
       end
     end
