0777d2a32b5e8ef1076bd039f73aa9c262d8eabf
Allen Taylor <aktaylor@sti-k12.com>
2016-06-15 15:52:10 -0500

    Remove equals from student mobile API

--- app/controllers/mobile/v1/students/classrooms_controller.rb
+++ app/controllers/mobile/v1/students/classrooms_controller.rb
@@ -1,6 +1,6 @@
 class Mobile::V1::Students::ClassroomsController < Mobile::V1::Students::BaseController
   def index
-    current_person = Student.find(181357)
+    #current_person = Student.find(181357)
     @classrooms = current_person.classrooms_for_school(current_school)   
     @recent_checking_amounts = PlutusAmountDecorator.decorate(Plutus::Amount.where(account_id: current_person.checking_account).joins(:transaction).order({ transaction: :created_at}))
     @recent_savings_amounts = PlutusAmountDecorator.decorate(Plutus::Amount.where(account_id: current_person.savings_account).joins(:transaction).order({ transaction: :created_at}))
