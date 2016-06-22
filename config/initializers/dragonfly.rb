234cdce184a887475713221b855c3ec542825549
Sonam Shah <sonam.shah@clariontechnologies.co.in>
2016-06-22 03:09:12 -0500

    Made changes to dragonfly configurations.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -11,25 +11,3 @@ app.datastore = Dragonfly::DataStorage::S3DataStore.new({
   region: 'us-west-2',
   url_scheme: 'https'
 })
-Dragonfly.app.configure do
-  # Override the .url method...
-  define_url do |app, job, opts|
-    thumb = Thumb.find_by_signature(job.signature)
-    # If (fetch 'some_uid' then resize to '40x40') has been stored already, give the datastore's remote url ...
-    if thumb
-      app.datastore.url_for(thumb.uid)
-    # ...otherwise give the local Dragonfly server url
-    else
-      app.server.url_for(job)
-    end
-  end
-
-  # Before serving from the local Dragonfly server...
-  before_serve do |job, env|
-    # ...store the thumbnail in the datastore...
-    uid = job.store
-    # ...keep track of its uid so next time we can serve directly from the datastore
-    Thumb.create!(uid: uid, signature: job.signature)
-  end
-
-end
