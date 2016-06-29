c7e8f1041a745e99d5e8fa415dd8a4dab53a8a16
Allen Taylor <aktaylor@sti-k12.com>
2016-06-29 11:30:00 -0500

    Auctions API to student mobile

---
+++ app/controllers/mobile/v1/students/auctions_controller.rb
@@ -0,0 +1,36 @@
+class Mobile::V1::Students::AuctionsController < Mobile::V1::Students::BaseController
+  def index
+    current_person = Student.find(181357)
+    @auctions = available_auctions
+    MixPanelTrackerWorker.perform_async(current_user.id, 'View Auctions', mixpanel_options)
+  end
+
+  def bid
+    current_person = Student.find(181357)    
+    if params[:id].blank? or params[:amount].blank?
+      @msg = " You must supply an id and amount to bid on an auction"
+      render json: { status:  :unprocessible_entity, msg: @msg} and return
+    end
+    auction = Auction.find(params[:id])
+    amount = SanitizingBigDecimal(params[:amount])
+
+    bid = BidOnAuctionCommand.new({ auction: auction, person: current_person, amount: amount, credit_manager: CreditManager.new })
+    bid.on_success = method(:on_success)
+    bid.on_failure = method(:on_failure)
+    bid.execute!
+  end 
+
+  def on_success(bid)
+    @msg = "Bid successful!"
+    render json: { status:  :ok, msg: @msg} 
+  end
+
+  def on_failure(bid)
+    @msg = bid.failure_reason.to_s.downcase.capitalize
+    render json: { status:  :unprocessible_entity, msg: @msg} 
+  end  
+  
+  def available_auctions
+    Auction.viewable_for(current_person)
+  end 
+end
