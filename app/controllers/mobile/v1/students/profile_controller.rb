6dc18b949c652e5e3b1aa4a590366cf40a7a60ab
Allen Taylor <aktaylor@sti-k12.com>
2016-07-05 06:53:21 -0500

    Add avatars to mobile student profile api

--- app/controllers/mobile/v1/students/profile_controller.rb
+++ app/controllers/mobile/v1/students/profile_controller.rb
@@ -1,12 +1,15 @@
 class Mobile::V1::Students::ProfileController < Mobile::V1::Students::BaseController
   def show
+    current_person = Student.find(181357)
     @student = Student.find(params[:id])
+    @avatars = Avatar.all
   end
 
   def update
     @student = Student.find(params[:id])
     @student.first_name = params[:student][:first_name]
     @student.last_name = params[:student][:last_name]
+    @student.avatar = Avatar.find(params[:avatar_id]) if !params[:avatar_id].blank?
     if @student.save
       render json: { status: :ok }
     else
