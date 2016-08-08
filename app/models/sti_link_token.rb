fcda4819f0071bd06f6da29e8a40d89ee34fdf80
Sonam Shah <sonam.shah@clariontechnologies.co.in>
2016-08-08 09:28:36 -0500

    Renamed Sti to Chalkable at Le admin.

--- app/models/sti_link_token.rb
+++ app/models/sti_link_token.rb
@@ -1,3 +1,4 @@
 class StiLinkToken < ActiveRecord::Base
   attr_accessible :api_url, :district_guid, :link_key, :username, :password, :status
+  attr_accessible :api_url, :district_guid, :link_key, :username, :password, :status, as: :admin
 end
