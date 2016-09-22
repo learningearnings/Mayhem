8d93db75c5c59b957ef74c5846c5f8c20907bf07
Sonam Shah <sonam.shah@clariontechnologies.co.in>
2016-09-22 09:01:05 -0500

    Removed all fields.

--- app/admin/audit_log.rb
+++ app/admin/audit_log.rb
@@ -10,30 +10,30 @@ ActiveAdmin.register AuditLog do
     # column "School sti_id" do |audit_log|
     #   audit_log.initiator.person.school.sti_id if !audit_log.initiator.person.is_a?(LeAdmin)
     # end
-    column "Person id" do |audit_log|
-      audit_log.initiator.person.id
-    end
-    column "Person sti_id" do |audit_log|
-      audit_log.initiator.person.sti_id if !audit_log.initiator.person.is_a?(LeAdmin)
-    end
-    column "Person Name" do |audit_log|
-      audit_log.initiator.person.name
-    end   
-    column "Person Type" do |audit_log|
-      audit_log.initiator.person.type
-    end
-    column "Object Type" do |audit_log|
-      audit_log.log_event_type
-    end
-    column "Object Name" do |audit_log|
-      if audit_log.log_event_type == 'Auction'
-        audit_log.log_event.product.name
-      elsif audit_log.log_event_type == "PersonSchoolLink" || audit_log.log_event_type == "PersonSchoolClassroomLink"
-        audit_log.log_event.person.name
-      else
-        audit_log.log_event.try(:name)
-      end  
-    end 
-    column "Date", :created_at
+    # column "Person id" do |audit_log|
+    #   audit_log.initiator.person.id
+    # end
+    # column "Person sti_id" do |audit_log|
+    #   audit_log.initiator.person.sti_id if !audit_log.initiator.person.is_a?(LeAdmin)
+    # end
+    # column "Person Name" do |audit_log|
+    #   audit_log.initiator.person.name
+    # end   
+    # column "Person Type" do |audit_log|
+    #   audit_log.initiator.person.type
+    # end
+    # column "Object Type" do |audit_log|
+    #   audit_log.log_event_type
+    # end
+    # column "Object Name" do |audit_log|
+    #   if audit_log.log_event_type == 'Auction'
+    #     audit_log.log_event.product.name
+    #   elsif audit_log.log_event_type == "PersonSchoolLink" || audit_log.log_event_type == "PersonSchoolClassroomLink"
+    #     audit_log.log_event.person.name
+    #   else
+    #     audit_log.log_event.try(:name)
+    #   end  
+    # end 
+    # column "Date", :created_at
   end
 end
--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,5 +1,5 @@
 class AuditLog < ActiveRecord::Base
   attr_accessible :user_id
-  belongs_to :log_event, polymorphic: true
-  belongs_to :initiator, :class_name => 'Spree::User', :foreign_key => :user_id
+  #belongs_to :log_event, polymorphic: true
+  #belongs_to :initiator, :class_name => 'Spree::User', :foreign_key => :user_id
 end
