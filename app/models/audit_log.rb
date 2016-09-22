199bbe9084b2d43582a0ae72dda8fa6ab04edce3
Sonam Shah <sonam.shah@clariontechnologies.co.in>
2016-09-22 09:33:10 -0500

    Removed all fields.

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -2,4 +2,5 @@ class AuditLog < ActiveRecord::Base
   attr_accessible :user_id
   belongs_to :log_event, polymorphic: true
   belongs_to :initiator, :class_name => 'Spree::User', :foreign_key => :user_id
+  self.inheritance_column = nil
 end
