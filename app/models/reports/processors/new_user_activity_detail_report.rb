31c03a046aac888098578020bcd45ed4707afaa2
Allen Taylor <aktaylor@sti-k12.com>
2016-10-03 13:26:38 -0500

    User activity detail report

--- app/models/reports/processors/new_user_activity_detail_report.rb
+++ app/models/reports/processors/new_user_activity_detail_report.rb
@@ -11,11 +11,11 @@ module Reports
       end
 
       def run
-        File.open("/tmp/teacher_logins" + filename, "w") {|f| f.write Reports::NewTeacherLoginsReport.new(options).run }
-        #File.open("/tmp/teacher_credits" + filename, "w") {|f| f.write Reports::NewTeacherCreditsReport.new(options).run }  
-        #File.open("/tmp/student_logins" + filename, "w") {|f| f.write Reports::NewStudentLoginsReport.new(options).run } 
-        #File.open("/tmp/student_credits" + filename, "w") {|f| f.write Reports::NewStudentCreditsReport.new(options).run }                     
-        AdminMailer.user_activity_detail_report(filename, email_recipients).deliver
+        File.open("/tmp/teacher_logins" + @filename, "w") {|f| f.write Reports::NewTeacherLoginsReport.new(options).run }
+        #File.open("/tmp/teacher_credits" + @filename, "w") {|f| f.write Reports::NewTeacherCreditsReport.new(options).run }  
+        #File.open("/tmp/student_logins" + @filename, "w") {|f| f.write Reports::NewStudentLoginsReport.new(options).run } 
+        #File.open("/tmp/student_credits" + @filename, "w") {|f| f.write Reports::NewStudentCreditsReport.new(options).run }                     
+        AdminMailer.user_activity_detail_report(filename, @email_recipients).deliver
       end
     end
   end
