992b6c7a79e85804b66fe2aa224085caecc0eae0
Allen Taylor <aktaylor@sti-k12.com>
2016-10-03 12:33:59 -0500

    User activity detail report

--- db/schema.rb
+++ db/schema.rb
@@ -93,17 +93,6 @@ ActiveRecord::Schema.define(:version => 20160930072632) do
     t.boolean  "fulfilled",                                      :default => false
     t.integer  "person_id"
     t.boolean  "canceled"
-    t.datetime "deleted_at"
-    t.string   "status"
-  end
-
-  create_table "audit_logs", :force => true do |t|
-    t.integer  "person_id"
-    t.integer  "log_event_id"
-    t.string   "log_event_type"
-    t.string   "action"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
   end
 
   create_table "avatars", :force => true do |t|
