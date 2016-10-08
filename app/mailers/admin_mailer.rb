2862d9f772b305fdc5d464cac1bcdbc58750f117
Allen Taylor <aktaylor@sti-k12.com>
2016-10-08 14:01:03 -0500

    District Summary report

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -30,6 +30,19 @@ class AdminMailer < ActionMailer::Base
     mail :to => recipients, :subject => "User Activity Detail Reports", :body => "User Activity Detail Reports"
   end
   
+  def district_dashboard_report filename, email_recipients
+    district_summary = "district_summary" + filename
+    #tcfn = "teacher_credits" + filename
+    #slfn = "student_logins" + filename
+    #scfn = "student_credits" + filename
+    attachments[district_summary] = File.read("/tmp/" + district_summary)
+    #attachments[tcfn] = File.read("/tmp/" + tcfn)
+    #attachments[slfn] = File.read("/tmp/" + slfn)
+    #attachments[scfn] = File.read("/tmp/" + scfn)           
+    recipients = ["aktaylor@chalkable.com"] + email_recipients.split(",") 
+    mail :to => recipients, :subject => "District Dashboard Report", :body => "District Dashboard Reports"
+  end  
+  
   def teacher_activity_report filename
     attachments[filename] = File.read("/tmp/" + filename)
     mail :to => ["aktaylor@chalkable.com","lrushing@chalkable.com", "jwood@sti-k12.com"], :subject => "Teacher Activity Report", :body => "Teacher Activity Report"
