fdc653853816e0da912650b3d2b1c93ee273461f
Allen Taylor <aktaylor@sti-k12.com>
2017-03-27 15:59:10 -0500

    Changed primary db

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :unicorn_rack_env, "production"
 role :web,    "192.241.249.30", "162.243.52.221", "107.170.98.188"
 role :sidekiq, "107.170.98.188"
 role :app,    "192.241.249.30", "162.243.52.221"
-#role :db,     "192.241.248.83", primary: true
+role :db,     "192.241.249.30", primary: true
