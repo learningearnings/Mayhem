f36cb73eaf6b37c4791d9258d19adc482448a06e
Allen Taylor <aktaylor@sti-k12.com>
2017-04-06 11:05:42 -0500

    Add version constant

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require File.expand_path('../boot', __FILE__)
 require 'pdfkit'
 require 'rails/all'
 
+LE_APP_VERSION = '2.5.0'
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
