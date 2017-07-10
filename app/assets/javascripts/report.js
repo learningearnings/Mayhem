6b6783e66a655566461fe12b4bd1435001374998
Swagata Biswas <swagatabiswasiitkgp@gmail.com>
2017-07-10 12:56:47 +0530

    Refactor

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -1,9 +1,7 @@
-$(document).ready(function(){
-	$('.print-report').on('click', function(){
-		var printContents = document.getElementsByClassName('printable-section')[0].innerHTML;
-		var originalContents = document.body.innerHTML;
-    document.body.innerHTML = printContents;
-    window.print();
-    document.body.innerHTML = originalContents;
-	});
-});
+function printReport(){
+  var printContents = document.getElementsByClassName('report')[0].innerHTML;
+  var originalContents = document.body.innerHTML;
+  document.body.innerHTML = printContents;
+  window.print();
+  document.body.innerHTML = originalContents;
+}
