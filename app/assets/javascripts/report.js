cf78c8aebbe89a499f85b5347188b19ea42abe0c
Swagata Biswas <swagatabiswasiitkgp@gmail.com>
2017-07-20 15:15:21 +0530

    Load header while pageload post print

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -2,8 +2,10 @@ function printReport(){
   var printBody = document.getElementsByClassName('report')[0].innerHTML;
   var printHeader = document.getElementsByTagName('h1')[0].innerHTML;
   var printContents = '<h1>' + printHeader + '</h1>' + printBody;
+  var originalHeader = document.head.innerHTML;
   var originalContents = document.body.innerHTML;
   document.body.innerHTML = printContents;
   window.print();
   document.body.innerHTML = originalContents;
+  document.head.innerHTML = originalHeader;
 }
