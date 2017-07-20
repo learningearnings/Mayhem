f44a6783c51c0fb559edc8b36d242c55e2175e27
Swagata Biswas <swagatabiswasiitkgp@gmail.com>
2017-07-20 15:56:49 +0530

    Open new window for printing report

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -2,10 +2,9 @@ function printReport(){
   var printBody = document.getElementsByClassName('report')[0].innerHTML;
   var printHeader = document.getElementsByTagName('h1')[0].innerHTML;
   var printContents = '<h1>' + printHeader + '</h1>' + printBody;
-  var originalHeader = document.head.innerHTML;
   var originalContents = document.body.innerHTML;
-  document.body.innerHTML = printContents;
-  window.print();
-  document.body.innerHTML = originalContents;
-  document.head.innerHTML = originalHeader;
+  w = window.open();
+  w.document.body.innerHTML = printContents;
+  w.print();
+  w.close();
 }
