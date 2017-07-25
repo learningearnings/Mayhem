71c73c9f2ea2695439ee18a4e553bfdfd187aa12
Swagata Biswas <swagatabiswasiitkgp@gmail.com>
2017-07-25 11:13:12 +0530

    Resolve merge conflict

--- app/controllers/sti_controller.rb
+++ app/controllers/sti_controller.rb
@@ -7,9 +7,9 @@ class StiController < ApplicationController
   skip_before_filter :subdomain_required
   skip_before_filter :verify_authenticity_token
   before_filter :handle_sti_token, :only => [:give_credits, :create_ebucks_for_students]
-  
+
   def sync_district
-    sti_link_token = StiLinkToken.where(district_guid: params[:district_guid].downcase).last      
+    sti_link_token = StiLinkToken.where(district_guid: params[:district_guid].downcase).last
     if sti_link_token.username == "PowerSchool"
       PSImporterWorker.setup_sync(district_guid: params[:district_guid].downcase)
     else
@@ -20,7 +20,7 @@ class StiController < ApplicationController
       @district.current_roster_version = nil
       @district.current_section_version = nil
       @district.save
-    
+
       StiImporterWorker.setup_sync(sti_link_token.api_url, "LearningEarnings",sti_link_token.password, @district.guid)
     end
     render :text => "Sync job submitted, check Sync Attempts for completion status"
@@ -30,19 +30,19 @@ class StiController < ApplicationController
     if current_school == nil or current_person == nil or current_person.main_account(current_school) == nil
       redirect_to main_app.page_path('home') and return
     end
-    if current_school.credits_scope != "School-Wide" 
+    if current_school.credits_scope != "School-Wide"
       if current_school.credits_type != "child"
-        redirect_to :action => "new_school_for_credits", :teacher => @teacher.id, :school => current_school.id and return 
+        redirect_to :action => "new_school_for_credits", :teacher => @teacher.id, :school => current_school.id and return
       else
         if params["studentIds"]
           @students = @current_school.students.where(sti_id: params["studentIds"].split(",")).order(:last_name, :first_name)
         else
-          @students = @current_school.students.order(:last_name, :first_name)          
+          @students = @current_school.students.order(:last_name, :first_name)
         end
       end
-    else     
-      load_students  
-    end  
+    else
+      load_students
+    end
     @teacher_email_form = TeacherEmailForm.new(:person_id => current_person.id)
     begin
       start_time = Time.now
@@ -57,7 +57,7 @@ class StiController < ApplicationController
     end
     render :layout => false
   end
-  
+
   def auth
     #Rails.logger.info("AKT: Enter auth with params: #{params.inspect}")
     if params["sti_session_variable"]
@@ -66,7 +66,7 @@ class StiController < ApplicationController
       if (sti_link_token == nil)
         flash[:error] = "Integrated sign in failed for district GUID #{params[:districtGUID]}; sti link token not found"
         return
-      end    
+      end
       sti_client = STI::Client.new :base_url => sti_link_token.api_url, :username => sti_link_token.username, :password => sti_link_token.password
       sti_client.session_token = params["sti_session_variable"]
       @client_response = sti_client.session_information.parsed_response
@@ -74,7 +74,7 @@ class StiController < ApplicationController
       if @client_response == nil or  ( @client_response["StaffId"].blank? and @client_response["StudentId"].blank? )
         flash[:error] = "Integrated sign in failed for district GUID #{params[:districtGUID]}; sti link client bad response"
         return
-      end  
+      end
       if @client_response["StaffId"].blank?
         @student = Student.where(district_guid: params[:districtGUID], sti_id: @client_response["StudentId"], status: "active").first
         if login_student
@@ -82,27 +82,23 @@ class StiController < ApplicationController
             redirect_to "/" and return
           else
             #Rails.logger.error("AKT Integrated sign in failed for district GUID, No school for logged in student")
-            flash[:error] = "Integrated sign in failed for district GUID, No school for logged in student"        
-            redirect_to "#{request.protocol}#{request.env["HTTP_HOST"]}" and return          
-          end   
+            flash[:error] = "Integrated sign in failed for district GUID, No school for logged in student"
+            redirect_to "#{request.protocol}#{request.env["HTTP_HOST"]}" and return
+          end
         else
-          #Rails.logger.error("AKT Integrated sign in failed for district GUID, Student login failed")        
-          flash[:error] = "Integrated sign in failed for district GUID #{params[:districtGUID]}, "  
-          return               
+          #Rails.logger.error("AKT Integrated sign in failed for district GUID, Student login failed")
+          flash[:error] = "Integrated sign in failed for district GUID #{params[:districtGUID]}, "
+          return
         end
       end
-      @teacher = Teacher.where(district_guid: params[:districtGUID], sti_id: @client_response["StaffId"], status: "active").first   
+      @teacher = Teacher.where(district_guid: params[:districtGUID], sti_id: @client_response["StaffId"], status: "active").first
       @schools = @teacher.schools.where(district_guid: params[:districtGUID], status: "active")
       if @schools and @schools.size > 1 and params[:sti_school_id].blank? and params[:schoolId].blank?
-        render partial: "teacher_choose_school", :locals => {:schools => @schools}        
-        return         
-      end    
+        render partial: "teacher_choose_school", :locals => {:schools => @schools}
+        return
+      end
       if login_teacher
-<<<<<<< HEAD
-        if current_school    
-=======
         if current_school
->>>>>>> origin/staging
           if @teacher.is_a?(SchoolAdmin)
             redirect_to main_app.school_admins_bank_path and return
           else
@@ -110,14 +106,14 @@ class StiController < ApplicationController
           end
         else
           Rails.logger.error("AKT Integrated sign in failed for district GUID, No school for logged in teacher")
-          flash[:error] = "Integrated sign in failed for district GUID, No school for logged in teacher"        
-          redirect_to "#{request.protocol}#{request.env["HTTP_HOST"]}" and return          
+          flash[:error] = "Integrated sign in failed for district GUID, No school for logged in teacher"
+          redirect_to "#{request.protocol}#{request.env["HTTP_HOST"]}" and return
         end
       else
-        Rails.logger.error("AKT Integrated sign in failed for district GUID, Teacher login failed")        
+        Rails.logger.error("AKT Integrated sign in failed for district GUID, Teacher login failed")
         flash[:error] = "Integrated sign in failed for district GUID #{params[:districtGUID]}, "
       end
-    else 
+    else
       if params[:schoolId].blank? and params[:sti_school_id].blank?
         flash[:error] = "Non integrated sign in failed -- Missing required parameter schoolId"
         return
@@ -126,23 +122,23 @@ class StiController < ApplicationController
         flash[:error] = "Non integrated sign in failed -- Missing required parameter districtGUID"
         return
       end
-      if params[:schoolId].blank?      
-        school = School.where(:district_guid => params[:districtGUID], :sti_id => params[:sti_school_id]).first 
+      if params[:schoolId].blank?
+        school = School.where(:district_guid => params[:districtGUID], :sti_id => params[:sti_school_id]).first
       else
-        school = School.where(:district_guid => params[:districtGUID], :sti_id => params[:schoolid]).first        
+        school = School.where(:district_guid => params[:districtGUID], :sti_id => params[:schoolid]).first
       end
       if !school
         flash[:error] = "School not found"
-        redirect_to main_app.page_path('home') and return        
-      end 
-      session[:current_school_id] = school.id      
+        redirect_to main_app.page_path('home') and return
+      end
+      session[:current_school_id] = school.id
       if params[:userid]
         teacher = school.teachers.detect { | teach | teach.sti_id == params[:userid].to_i }
         if teacher
           if teacher.user.confirmed_at.nil?
             teacher.user.confirmed_at = Time.now
             teacher.user.save
-          end          
+          end
           sign_in(teacher.user)
           redirect_to "/" and return
         end
@@ -151,40 +147,40 @@ class StiController < ApplicationController
           if student.user.confirmed_at.nil?
             teacher.user.confirmed_at = Time.now
             teacher.user.save
-          end          
+          end
           sign_in(student.user)
           redirect_to "/" and return
-        end    
-        flash[:error] = "User not found"    
-        redirect_to main_app.page_path('home') and return   
+        end
+        flash[:error] = "User not found"
+        redirect_to main_app.page_path('home') and return
       else
         flash[:error] = "Non integrated sign in failed -- Missing required parameters"
       end
     end
   end
-  
+
   def save_teacher
     @teacher_email_form = TeacherEmailForm.new(params[:teacher])
     @teacher_email_form.save
     redirect_to :action => :give_credits
-  end 
-    
+  end
+
   def new_school_for_credits
     @teacher = Teacher.find(params[:teacher])
     @school = School.find(params[:school])
     @new_school_form = NewSchoolForm.new
     render :layout => false
   end
-  
+
   def save_school_for_credits
     @teacher = Teacher.find(params[:school][:teacher])
     @school = School.find(params[:school][:school])
     @new_school_form = NewSchoolForm.new(params[:school])
     if @new_school_form.save(@school,@teacher)
-      @current_school = @new_school_form.school  
-      @current_person = @new_school_form.teacher    
+      @current_school = @new_school_form.school
+      @current_person = @new_school_form.teacher
       request.env["devise.skip_trackable"] = true
-      sign_in(@new_school_form.teacher.user)      
+      sign_in(@new_school_form.teacher.user)
       session[:current_school_id] = @current_school.id
       @students = []
       if params["studentIds"]
@@ -198,16 +194,16 @@ class StiController < ApplicationController
       render :new_school_for_credits
     end
   end
-  
+
   def begin_le_tour
     @current_school = School.find(params[:sid])
     @current_person = @current_school.teachers.first
-    sign_in(@current_person.user)    
-    session[:current_school_id] = @current_school.id 
-    session[:tour] = "Y"    
-    redirect_to "/?tour=Y"  
-  end 
-  
+    sign_in(@current_person.user)
+    session[:current_school_id] = @current_school.id
+    session[:tour] = "Y"
+    redirect_to "/?tour=Y"
+  end
+
   def sync
     @link = StiLinkToken.where(district_guid: params[:district_guid], status: 'active').first
     if @link
@@ -253,11 +249,11 @@ class StiController < ApplicationController
     link_status
   end
 
-  def load_students    
+  def load_students
     if params["studentIds"]
       @students = current_school.students.where(district_guid: params[:districtGUID], sti_id: params["studentIds"].split(",")).order(:last_name, :first_name)
     else
-      @students = current_school.students.where(district_guid: params[:districtGUID]).order(:last_name, :first_name)      
+      @students = current_school.students.where(district_guid: params[:districtGUID]).order(:last_name, :first_name)
     end
   end
 
@@ -274,36 +270,36 @@ class StiController < ApplicationController
   def person
     current_person
   end
-  
+
   def login_student
     Rails.logger.info("AKT Login student: #{@client_response["StudentId"]}, district_guid: #{params[:districtGUID]}, sti_school_id: #{params[:sti_school_id]}, schoolId: #{params[:schoolId]}")
     @student = Student.where(district_guid: params[:districtGUID], sti_id: @client_response["StudentId"], status: "active").first
     Rails.logger.info("AKT Login student: #{@student.inspect}")
-    return false if @student.nil?   
+    return false if @student.nil?
     if params[:sti_school_id]
       school = School.where(district_guid: params[:districtGUID], sti_id: params[:sti_school_id], status: "active").first
     elsif params[:schoolId]
       school = School.where(district_guid: params[:districtGUID], sti_id: params[:schoolId], status: "active").first
     else
       sid = @student.person_school_links.order('created_at desc').first.try(:school_id)
-      school = School.where(id: sid).first if sid  
-    end  
+      school = School.where(id: sid).first if sid
+    end
     if school
       session[:current_school_id] = school.id
       school = school
-      @current_school = school 
+      @current_school = school
     else
       Rails.logger.error("AKT Login student, no school found")
-      return false    
-    end   
+      return false
+    end
     Rails.logger.info("AKT Login school: #{school.inspect}")
     if @student.user.confirmed_at.nil?
       @student.user.confirmed_at = Time.now
       @student.user.save
     end
-    sign_in(@student.user)  
+    sign_in(@student.user)
     Rails.logger.info("AKT: Student signed in..")
-    return true  
+    return true
   end
 
   def login_teacher
@@ -316,10 +312,10 @@ class StiController < ApplicationController
     elsif params[:schoolId]
       school = @teacher.schools.where(district_guid: params[:districtGUID], sti_id: params[:schoolId], status: "active").first
     else
-      school = @teacher.schools.where(district_guid: params[:districtGUID], status: "active").first    
+      school = @teacher.schools.where(district_guid: params[:districtGUID], status: "active").first
     end
     if school
-      session[:current_school_id] = school.id 
+      session[:current_school_id] = school.id
       @current_school = school
     end
     Rails.logger.info("AKT Login school: #{school.inspect}")
@@ -346,32 +342,32 @@ class StiController < ApplicationController
         if student_school
           session[:current_school_id] = sid
           school = student_school
-          @current_school = school     
+          @current_school = school
         end
       else
         Rails.logger.error("AKT Student not found with sti_id #{student_sti_id} for district #{params[:districtGUID]}")
       end
     end
-    if school and school.credits_scope != "School-Wide" 
+    if school and school.credits_scope != "School-Wide"
       @child = School.where(sti_id: school.id, credits_type: "child")
       if @child.size > 0
-        @current_school = @child[0]        
+        @current_school = @child[0]
         @teacher = @current_school.teachers.first
         if @teacher
-          session[:current_school_id] = @child[0].id          
+          session[:current_school_id] = @child[0].id
           @current_person = @teacher
-          sign_in(@teacher.user)  
+          sign_in(@teacher.user)
         else
           Rails.logger.error("AKT: School #{school.id} with credits scope: #{school.credits_scope} has no child, district_guid:  #{params[:districtGUID]}")
-          flash[:error] = "School #{school.id} with credits scope: #{school.credits_scope} has no child, district_guid: #{params[:districtGUID]}"         
+          flash[:error] = "School #{school.id} with credits scope: #{school.credits_scope} has no child, district_guid: #{params[:districtGUID]}"
           return false
-        end      
-      end   
-    end    
+        end
+      end
+    end
     if !school
       Rails.logger.error("AKT Login Teacher, no school found")
       return false
-    end    
+    end
     return true
   end
 
@@ -379,12 +375,12 @@ class StiController < ApplicationController
     sti_link_token = StiLinkToken.where(:district_guid => params[:districtGUID], status: 'active').last
     if (sti_link_token == nil)
       return false
-    end    
+    end
     sti_client = STI::Client.new :base_url => sti_link_token.api_url, :username => sti_link_token.username, :password => sti_link_token.password
     sti_client.session_token = params["sti_session_variable"]
     @client_response = sti_client.session_information.parsed_response
     if @client_response == nil || @client_response["StaffId"].blank? || !login_teacher
-      render partial: "teacher_not_found"        
+      render partial: "teacher_not_found"
       return false
     end
     return true
