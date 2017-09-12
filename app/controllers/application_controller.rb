2450cbc729e6675818d626f041269e0d1322a4af
Allen Taylor <aktaylor@sti-k12.com>
2017-09-12 15:51:21 -0500

    update redis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,11 +46,11 @@ class ApplicationController < ActionController::Base
   end
   
   def check_mixpanel
-    if !session[:mixpanelinit] and current_user
-      MixPanelIdentifierWorker.perform_async(current_user.id, mixpanel_options)
-      MixPanelTrackerWorker.perform_async(current_user.id, 'User Login', mixpanel_options)
+    #if !session[:mixpanelinit] and current_user
+    #  MixPanelIdentifierWorker.perform_async(current_user.id, mixpanel_options)
+    #  MixPanelTrackerWorker.perform_async(current_user.id, 'User Login', mixpanel_options)
       session[:mixpanelinit] = true
-
+    #
     end
   end
   
