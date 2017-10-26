ad01f8761841ab284a3f942661170431ae8e4709
Allen Taylor <aktaylor@sti-k12.com>
2017-10-26 10:03:19 -0500

    demo env

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,7 +1,7 @@
 set :rails_env, "demo"
 set :branch,  "demo"
 set :unicorn_rack_env, "demo"
-role :web,    "162.243.9.20"
-role :app,    "162.243.9.20"
-role :db,     "162.243.9.20", primary: true
-role :sidekiq, "162.243.9.20"
+role :web,    "18.220.199.6"
+role :app,    "18.220.199.6"
+role :db,     "18.220.199.6", primary: true
+role :sidekiq, "18.220.199.6"
